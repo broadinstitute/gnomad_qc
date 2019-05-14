331e81fd04f424d8ef0cce67f0096e597e37c999
konradjk <konradjkarczewski@gmail.com>
2019-05-14 16:35:13 -0400

    addressed comments

--- annotations/generate_ld_data.py
+++ annotations/generate_ld_data.py
@@ -20,14 +20,10 @@ def ld_scores_path(data_type: str, pop: str, version: str = CURRENT_RELEASE):
     return f'gs://gnomad-resources/ld/gnomad.{data_type}.r{version}.{pop}.ld_scores.ht'
 
 
-eur_w_ld_chr_ht = 'gs://konradk/ld_score/reference_data/eur_w_ld_chr/20.l2.ldscore.ht'
-eur_ref_ld_chr_ht = 'gs://konradk/ld_score/reference_data/eur_ref_ld_chr/20.l2.ldscore.ht'
-kg_ld_ht = 'gs://konradk/ld_score/reference_data/1000G_Phase3_baselineLD_v2.1_ldscores/baselineLD.20.l2.ldscore.ht'
-
-
 def get_pop_and_subpop_counters(mt):
-    cut_dict = {'pop': hl.agg.filter(hl.is_defined(mt.meta.pop), hl.agg.counter(mt.meta.pop)),
-                'subpop': hl.agg.filter(hl.is_defined(mt.meta.subpop), hl.agg.counter(mt.meta.subpop))
+    cut_dict = {'pop': hl.agg.filter(hl.is_defined(mt.meta.pop) & (mt.meta.pop != 'oth'), hl.agg.counter(mt.meta.pop)),
+                'subpop': hl.agg.filter(hl.is_defined(mt.meta.subpop) & (mt.meta.subpop != 'oea') &
+                                        (mt.meta.subpopt != 'onf'), hl.agg.counter(mt.meta.subpop))
                 }
     cut_data = mt.aggregate_cols(hl.struct(**cut_dict))
     return cut_data
@@ -40,29 +36,17 @@ def filter_mt_for_ld(mt, label, pop):
     pop_mt = pop_mt.filter_rows((hl.len(pop_mt.filters) == 0) &
                                 (pop_mt.pop_freq.AC > 1) &
                                 (pop_mt.pop_freq.AN - pop_mt.pop_freq.AC > 1) &
+                                #  the following filter is needed for "het-only" monomorphic sites
+                                #  as otherwise variance is 0, and so, row_correlation errors out
                                 ~((pop_mt.pop_freq.AF == 0.5) &
                                   (pop_mt.pop_freq.homozygote_count == 0)))
     return pop_mt
-
-
-def import_reference_ld_scores():
-    ht = hl.import_table(eur_w_ld_chr_ht.replace('.ht', '.gz'), impute=True, types={'CHR': hl.tstr})
-    ht.key_by(locus=hl.locus(ht.CHR, ht.BP)).write(eur_w_ld_chr_ht)
-
-    ht = hl.import_table(eur_ref_ld_chr_ht.replace('.ht', '.gz'), impute=True, types={'CHR': hl.tstr})
-    ht.key_by(locus=hl.locus(ht.CHR, ht.BP)).write(eur_ref_ld_chr_ht)
-
-    ht = hl.import_table(kg_ld_ht.replace('.ht', '.gz'), impute=True, types={'CHR': hl.tstr})
-    ht = ht.select('CHR', 'BP', 'SNP', L2=ht.baseL2)
-    ht.key_by(locus=hl.locus(ht.CHR, ht.BP)).write(kg_ld_ht)
 
 
 def generate_ld_pruned_set(mt: hl.MatrixTable, pop_data: dict, data_type: str, r2: str = '0.2',
                            radius: int = 1000000, overwrite: bool = False):
     for label, pops in dict(pop_data).items():
         for pop in pops:
-            if pop in ('oth', 'onf', 'oea'): continue
-
             pop_mt = filter_mt_for_ld(mt, label, pop)
 
             pruned_ht = hl.ld_prune(pop_mt.GT, r2=float(r2), bp_window_size=radius)
@@ -75,8 +59,6 @@ def generate_ld_pruned_set(mt: hl.MatrixTable, pop_data: dict, data_type: str, r
 def generate_ld_matrix(mt, pop_data, data_type, radius: int = 1000000, overwrite: bool = False):
     for label, pops in dict(pop_data).items():
         for pop in pops:
-            if pop in ('oth', 'onf', 'oea'): continue
-
             pop_mt = filter_mt_for_ld(mt, label, pop)
 
             pop_mt.rows().select('pop_freq').add_index().write(ld_index_path(data_type, pop), overwrite)
@@ -89,7 +71,6 @@ def generate_ld_scores_from_ld_matrix(pop_data, data_type, min_frequency=0.01, c
                                       overwrite=False):
     for label, pops in dict(pop_data).items():
         for pop, n in pops.items():
-            if pop in ('oth', 'onf', 'oea'): continue
             ht = hl.read_table(ld_index_path(data_type, pop))
             ht = ht.filter((ht.pop_freq.AF >= min_frequency) &
                            (ht.pop_freq.AF <= 1 - min_frequency) &
@@ -102,6 +83,9 @@ def generate_ld_scores_from_ld_matrix(pop_data, data_type, min_frequency=0.01, c
             r2_adj = ((n - 1.0) / (n - 2.0)) * r2 - (1.0 / (n - 2.0))
 
             starts_and_stops = hl.linalg.utils.locus_windows(ht.locus, radius, _localize=False)
+
+            # Lifted directly from https://github.com/hail-is/hail/blob/555e02d6c792263db2c3ed97db8002b489e2dacb/hail/python/hail/methods/statgen.py#L2595
+            # for the time being, until efficient BlockMatrix filtering gets an easier interface
             r2_adj = BlockMatrix._from_java(r2_adj._jbm.filterRowIntervalsIR(
                 Env.backend()._to_java_ir(starts_and_stops._ir),
                 False))
