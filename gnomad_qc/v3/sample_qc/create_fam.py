996fd12514b37798683d1cf8e0e6353a51fe8569
laurent <lfran@broadinstitute.org>
2020-02-25 17:03:35 -0500

    Moved `create_fake_pedigree` to gnomad_hail

--- gnomad_qc/v3/sample_qc/create_fam.py
+++ gnomad_qc/v3/sample_qc/create_fam.py
@@ -1,8 +1,6 @@
-from typing import List
 from collections import defaultdict, Counter
 import argparse
 import hail as hl
-import random
 from gnomad_qc.v3.resources import (
     meta,
     pedigree,
@@ -19,57 +17,8 @@ from gnomad_hail.utils.relatedness import (
     get_duplicated_samples,
     get_duplicated_samples_ht,
     infer_families,
+    create_fake_pedigree,
 )
-
-
-def create_fake_pedigree(
-    n: int, sample_list: List[str], real_pedigree: hl.Pedigree = None
-) -> hl.Pedigree:
-    """
-
-    Generates a "fake" pedigree made of trios created by sampling 3 random samples in the sample list.
-    If `real_pedigree` is given, then children from the real pedigrees won't be used as probands.
-    This functions insures that:
-    - All probands are unique
-    - All individuals in a trio are different
-
-    :param int n: Number of fake trios desired in the pedigree
-    :param list of str sample_list: List of samples
-    :param Pedigree real_pedigree: Optional pedigree to exclude children from
-    :return: Fake pedigree
-    :rtype: Pedigree
-    """
-
-    probands = set()
-    if real_pedigree is not None:
-        probands = {trio.s for trio in real_pedigree.trios}.intersection(
-            set(sample_list)
-        )
-        if len(probands) == len(sample_list):
-            raise ValueError(
-                "Full sample list for fake trios generation needs to include samples that aren't probands in the real trios."
-            )
-
-    fake_trios = []
-    for i in range(n):
-        mat_id, pat_id = random.sample(sample_list, 2)
-        s = random.choice(sample_list)
-        while s in probands.union({mat_id, pat_id}):
-            s = random.choice(sample_list)
-
-        probands.add(s)
-
-        fake_trios.append(
-            hl.Trio(
-                s=s,
-                pat_id=pat_id,
-                mat_id=mat_id,
-                fam_id=f"fake_{str(i+1)}",
-                is_female=True,
-            )
-        )
-
-    return hl.Pedigree(fake_trios)
 
 
 def run_mendel_errors() -> hl.Table:
