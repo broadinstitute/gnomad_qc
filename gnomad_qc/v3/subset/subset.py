bfe6bda832a1c7a9caa7f01cbd8c36c29e5b7cf9
Mike Wilson <mwilson@broadinstitute.org>
2020-03-12 12:18:23 -0400

    Initial subset script for v3

---
+++ gnomad_qc/v3/subset/subset.py
@@ -0,0 +1,141 @@
+import argparse
+import logging
+import hail as hl
+from gnomad_qc.v3.resources.meta import meta as metadata
+from gnomad_qc.v3.resources.annotations import get_info
+from gnomad_qc.v3.resources.raw import get_gnomad_v3_mt
+from gnomad.resources.grch38.gnomad import GENOME_POPS
+
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s: %(message)s",
+    datefmt="%m/%d/%Y %I:%M:%S %p",
+)
+logger = logging.getLogger("seqr_sample_qc")
+logger.setLevel(logging.INFO)
+
+
+def subset(mt, subset_list):
+    subset_count = subset_list.count()
+    logger.info(f"Subsetting {subset_count} samples")
+    anti_join_ht = subset_list.anti_join(mt.cols())
+    anti_join_ht_count = anti_join_ht.count()
+
+    if anti_join_ht_count != 0:
+        missing_samples = anti_join_ht.s.collect()
+        raise ValueError(
+            f"Only {subset_count-anti_join_ht_count} out of {subset_count} "
+            "subsetting-table IDs matched IDs in the matrix table. \n"
+            f"IDs that aren't in the matrix table: {missing_samples} \n"
+            f"All callset sample IDs:{mt.s.collect()}",
+            missing_samples,
+        )
+    mt = mt.semi_join_cols(subset_list)
+
+    return mt
+
+
+def format_info_for_vcf(info_ht) -> hl.Table:
+    logger.info(f"Formatting the info hail table for VCF export")
+    for f, ft in info_ht.info.dtype.items():
+        if ft == hl.dtype("int64"):
+            info_ht = info_ht.annotate(
+                info=info_ht.info.annotate(
+                    **{f: hl.int32(hl.min(2 ** 31 - 1, info_ht.info[f]))}
+                )
+            )
+        elif ft == hl.dtype("float64"):
+            info_ht = info_ht.annotate(
+                info=info_ht.info.annotate(
+                    **{f: hl.float32(hl.min(2 ** 31 - 1, info_ht.info[f]))}
+                )
+            )
+        elif ft == hl.dtype("array<int64>"):
+            info_ht = info_ht.annotate(
+                info=info_ht.info.annotate(
+                    **{
+                        f: info_ht.info[f].map(
+                            lambda x: hl.int32(hl.min(2 ** 31 - 1, x))
+                        )
+                    }
+                )
+            )
+        elif ft == hl.dtype("array<float64>"):
+            info_ht = info_ht.annotate(
+                info=info_ht.info.annotate(
+                    **{
+                        f: info_ht.info[f].map(
+                            lambda x: hl.float32(hl.min(2 ** 31 - 1, x))
+                        )
+                    }
+                )
+            )
+        elif ft == hl.dtype("array<array<int32>>"):
+            info_ht = info_ht.annotate(
+                info=info_ht.info.annotate(
+                    **{f: hl.flatmap(lambda x: x, info_ht.info[f])}
+                )
+            )
+    return info_ht
+
+
+def main(args):
+    hl.init(
+        log="/subset.log", tmp_dir="hdfs:///subset.tmp/", default_reference="GRCh38"
+    )
+    pop = args.pop
+    sample_subset = hl.import_table(args.subset, key="s")
+    mt = get_gnomad_v3_mt(
+        key_by_locus_and_alleles=True, remove_hard_filtered_samples=False
+    )
+    info_ht = get_info().ht()
+    info_ht = format_info_for_vcf(info_ht)
+    meta = hl.read_table(metadata)
+
+    if pop and pop in GENOME_POPS:
+        logger.info("Subsetting samples to {pop} population")
+        mt = mt.annotate_cols(pop=meta[mt.col_key].pop)
+        mt = mt.filter_cols(mt.pop == pop)
+        mt = mt.cols().drop('pop')
+
+    if args.subset:
+        mt = subset(mt, sample_subset)
+
+    mt = hl.experimental.sparse_split_multi(mt, filter_changed_loci=True)
+    mt = hl.experimental.densify(mt)
+
+    mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
+    mt = mt.filter_rows(hl.len(mt.alleles) > 1)
+
+    mt = mt.drop(mt.gvcf_info)
+    mt = mt.annotate_rows(info=info_ht[mt.row_key].info)
+
+    mt = mt.naive_coalesce(1000)
+    hl.export_vcf(
+        mt, args.output_path, parallel="header_per_shard"
+    )
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(
+        description="This script subsets gnomAD using a list of samples or population"
+    )
+
+    parser.add_argument(
+        "-s", "--subset", help="Path to subset table of sample IDs with header: s"
+    )
+    parser.add_argument("-pop", help="Subset from specific population")
+    parser.add_argument(
+        "--output-path", help="Output file path for subsetted VCF", required=True,
+    )
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        help="Overwrite all data from this subset (default: False)",
+        action="store_true",
+    )
+    args = parser.parse_args()
+
+    main(args)
