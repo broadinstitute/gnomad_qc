e15eb17fe8be77697933a4c05122b1b36a29b32e
laurent <lfran@broadinstitute.org>
2020-08-17 16:22:46 -0400

    Updated coverage version

--- gnomad_qc/v3/load_data/compute_coverage.py
+++ gnomad_qc/v3/load_data/compute_coverage.py
@@ -12,11 +12,14 @@ def main(args):
     hl.init(default_reference='GRCh38')
     logger = logging.getLogger("gnomad_qc.v3.load_data.compute_coverage")
 
+    logger.warning("Last time this was run (July 2020), this script required high-mem machines.")
+
     if args.compute_coverage_ht:
 
         print("Building reference context HT")
         ref_ht = get_reference_ht(
             hl.get_reference('GRCh38'),
+            contigs=[f'chr{x}' for x in range(1,23)] + ['chrX', 'chrY'],
             excluded_intervals=telomeres_and_centromeres.ht().interval.collect()
         )
         ref_ht = ref_ht.checkpoint("gs://gnomad-tmp/ref_context.ht", overwrite=True)
@@ -33,13 +36,13 @@ def main(args):
         coverage_ht = coverage_ht.checkpoint('gs://gnomad-tmp/gnomad.genomes_v3.coverage.summary.ht', overwrite=True)
         coverage_ht = coverage_ht.naive_coalesce(5000)
 
-        coverage_ht.write(coverage('genomes').versions['3.0'].path, overwrite=args.overwrite)
+        coverage_ht.write(coverage('genomes').versions['3.0.1'].path, overwrite=args.overwrite)
 
     if args.export_coverage:
-        ht = coverage('genomes').versions['3.0'].ht()
+        ht = coverage('genomes').versions['3.0.1'].ht()
         if 'count_array' in ht.row_value: # Note that count_array isn't computed any more, so this is v3.0-specific
             ht = ht.drop('count_array')
-        ht.export(coverage_tsv_path('genomes', '3.0'))
+        ht.export(coverage_tsv_path('genomes', '3.0.1'))
 
 
 if __name__ == "__main__":
