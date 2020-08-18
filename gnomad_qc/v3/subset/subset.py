43f388e4b0e6f45f93b75cb9467e3b58a8d10380
Mike Wilson <mwilson@broadinstitute.org>
2020-08-18 13:25:32 -0400

    Add subset  and gnomad callstats

--- gnomad_qc/v3/subset/subset.py
+++ gnomad_qc/v3/subset/subset.py
@@ -1,11 +1,14 @@
 import argparse
 import logging
+import sys
+
 import hail as hl
+
 from gnomad_qc.v3.resources.meta import meta as metadata
 from gnomad_qc.v3.resources.annotations import get_info
 from gnomad_qc.v3.resources.raw import get_gnomad_v3_mt
 from gnomad.resources.grch38.gnomad import GENOME_POPS
-from gnomad.utils.generic import subset_samples_and_variants
+from gnomad.utils.filtering import subset_samples_and_variants
 
 
 logging.basicConfig(
@@ -13,7 +16,7 @@ logging.basicConfig(
     format="%(asctime)s: %(message)s",
     datefmt="%m/%d/%Y %I:%M:%S %p",
 )
-logger = logging.getLogger("seqr_sample_qc")
+logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 HEADER_DICT = {
@@ -113,7 +116,7 @@ def format_info_for_vcf(ht) -> hl.Table:
     for f, ft in ht.info.dtype.items():
         if ft == hl.dtype("int64"):
             ht = ht.annotate(
-                info=ht.info.annotate(**{f: hl.int32(hl.min(2 ** 31 - 1, ht.info[f]))})
+                info=ht.info.annotate(**{f: hl.int32(hl.min(2 ** 31 - 1, ht.info[f]))}) # add warning that number being capped
             )
         elif ft == hl.dtype("float64"):
             ht = ht.annotate(
@@ -149,19 +152,22 @@ def compute_partitions(mt, entry_size=3.5, partition_size=128000000) -> int:
     rows, columns = mt.count()
     mt_disk_est = rows * columns * entry_size
     n_partitions = (
-        int(mt_disk_est / partition_size) if (mt_disk_est / partition_size) > 20 else 20
+        int(mt_disk_est / partition_size) if (mt_disk_est / partition_size) > 20 else 20 # make max
     )
-    return n_partitions
+    return n_partitions # bring to gnomad_methods -> parameterize
 
 
 def main(args):
-    hl.init(
-        log="/subset.log", default_reference="GRCh38"
-    )
+    hl.init(log="/subset.log", default_reference="GRCh38")
     pop = args.pop
     subset = args.subset
+    hard_filter = args.hard_filter_samples
+    release = args.release
+    sparse = args.sparse
+    header_dict = HEADER_DICT
+
     mt = get_gnomad_v3_mt(
-        key_by_locus_and_alleles=True, remove_hard_filtered_samples=False
+        key_by_locus_and_alleles=True, remove_hard_filtered_samples=hard_filter, release_only=release
     )
     info_ht = get_info().ht()
     info_ht = format_info_for_vcf(info_ht)
@@ -180,14 +186,18 @@ def main(args):
         mt = mt.filter_cols(meta[mt.col_key].pop == pop)
 
     if subset:
-        mt = subset_samples_and_variants(mt, subset, sparse=True, gt_expr="LGT")
+        gt_expr = "LGT" if sparse else "GT" #assumes matrix table is not split - infer 
+        mt = subset_samples_and_variants(mt, subset, sparse=sparse, gt_expr=gt_expr)
 
-    if args.pull_meta:
+    if args.add_meta:
         meta = meta.semi_join(mt.cols())
         meta.export(f"{args.output_path}/metadata.tsv.bgz")
 
-    mt = hl.experimental.sparse_split_multi(mt, filter_changed_loci=True)
-    mt = hl.experimental.densify(mt)
+    if sparse:
+        mt = hl.experimental.sparse_split_multi(mt, filter_changed_loci=True)
+        mt = hl.experimental.densify(mt)
+    else:
+        mt = hl.split_multi_hts(mt)
 
     mt = mt.filter_rows(hl.len(mt.alleles) > 1)
     mt = mt.filter_rows(hl.agg.any(mt.GT.is_non_ref()))
@@ -195,12 +205,40 @@ def main(args):
     mt = mt.drop(mt.gvcf_info)
     mt = mt.annotate_rows(info=info_ht[mt.row_key].info)
 
+    if args.subset_call_stats:
+        mt = mt.annotate_rows(subset_callstats=hl.agg.call_stats(mt.GT,mt.alleles))     
+        mt = mt.annotate_rows(info=mt.info.annotate(subset_AC=mt.subset_callstats.AC[1],
+                                           subset_AN=mt.subset_callstats.AN,
+                                           subset_AF=(mt.subset_callstats.AF[1].map(lambda x: hl.float32(hl.min(2 ** 31 - 1, x))))))
+        mt = mt.drop('subset_callstats')
+        header_dict = header_dict['info'].update({"subset_AC":"Alternate Allele count in subset post-filtering", 
+                                                "subset_AN":"Ref and Alternate allele number in subset post-filtering",
+                                                "subset_AF":"Alternate Allele frequency in subset post-filtering"})
+    
+    if args.add_gnomad_freqs:
+        from gnomad_qc.v3.resources.annotations import freq
+        freq = freq.ht()
+        mt = mt.annotate_rows(info=mt.info.annotate(gnomAD_AC_adj=freq[mt.row_key].freq[0].AC, 
+                                                    gnomAD_AN_adj=freq[mt.row_key].freq[0].AN,
+                                                    gnomAD_AF_adj=hl.float32(freq[mt.row_key].freq[0].AF)),
+                                                    gnomAD_AC_raw=freq[mt.row_key].freq[1].AC, 
+                                                    gnomAD_AN_raw=freq[mt.row_key].freq[1].AN,
+                                                    gnomAD_AF_raw=hl.float32(freq[mt.row_key].freq[1].AF))
+        header_dict = header_dict['info'].update({"gnomAD_AC_adj":"High quality alternate allele count in gnomAD post-filtering", 
+                                                "gnomAD_AN_adj":"High quality allele number. The total number of called alleles in gnomAD post-filtering",
+                                                "gnomAD_AF_adj":"High quality alternate allele frequency in gnomAD post-filtering",
+                                                "gnomAD_AC_raw":"Raw alternate allele count in gnomAD post-filtering", 
+                                                "gnomAD_AN_raw":"Raw allele number. The total number of called alleles in gnomAD post-filtering",
+                                                "gnomAD_AF_raw":"Raw alternate allele frequency in gnomAD post-filtering"})
+
     partitions = (
         compute_partitions(mt) if not args.num_vcf_shards else args.num_vcf_shards
     )
     logger.info(f"Naive coalescing to {partitions}")
 
     mt = mt.naive_coalesce(partitions)
+    if args.checkpoint_path:
+        mt = mt.checkpoint(args.checkpoint_path, overwrite=True)
 
     logger.info("Exporting VCF")
     hl.export_vcf(
@@ -220,16 +258,26 @@ if __name__ == "__main__":
     parser.add_argument(
         "-s", "--subset", help="Path to subset table of sample IDs with header: s"
     )
-    parser.add_argument("-pop", help="Subset from specific population")
+    parser.add_argument("--pop", help="Subset from specific population") # add choices for gnomad pops
     parser.add_argument(
-        "--pull-meta", help="Pull sample subset metadata", action="store_true"
+        "--add-meta", help="Pull sample subset metadata", action="store_true"
     )
     parser.add_argument(
-        "--output-path", help="Output file path for subsetted VCF", required=True,
+        "--output-path", help="Output file path for subsetted VCF, do not include file", required=True,
     )
     parser.add_argument(
-        "--num-vcf-shards", help="Number of shards in output VCF", type=int
+        "--num-vcf-shards", help="Number of shards in output VCF", type=int # clarify docstring
     )
+    parser.add_argument("--checkpoint-path", help="Path to final mt checkpoint")
+    parser.add_argument("--hard-filter-samples", help="Removes hard filtered samples", required=True, action="store_true")
+    parser.add_argument("--release", help="Keep release samples only", required=True, action="store_true")
+    parser.add_argument(
+        "--subset-call-stats", help="Adds subset callstats, AC, AN, AF"
+    )
+    parser.add_argument(
+        "--add-gnomad-freqs", help="Adds gnomAD's adj and raw callstats"
+    )
+    parser.add_argument("--sparse", help="Use if source MT is sparse", action="store_true")
     parser.add_argument(
         "-o",
         "--overwrite",
@@ -238,4 +286,10 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
+    if not (args.subset or args.pop):
+        sys.exit('Error: At least subset path or population must be specified')
+
     main(args)
+
+
+#Do we want to pass in non-gnomAD mt?
