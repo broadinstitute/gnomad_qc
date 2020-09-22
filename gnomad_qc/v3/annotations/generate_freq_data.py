466bbd65064946e616a9ee5a8063b25cd7d936b3
jkgoodrich <jgoodric@broadinstitute.org>
2020-09-22 10:01:44 -0600

    remove unneeded import

--- gnomad_qc/v3/annotations/generate_freq_data.py
+++ gnomad_qc/v3/annotations/generate_freq_data.py
@@ -1,7 +1,7 @@
 from gnomad.utils.annotations import annotate_freq, qual_hist_expr, pop_max_expr, faf_expr
 from gnomad.utils.annotations import get_adj_expr
 from gnomad.sample_qc.sex import adjusted_sex_ploidy_expr
-from gnomad_qc.v3.resources import get_gnomad_v3_mt, get_info, meta, freq
+from gnomad_qc.v3.resources import get_gnomad_v3_mt, meta, freq
 import argparse
 import logging
 import hail as hl
