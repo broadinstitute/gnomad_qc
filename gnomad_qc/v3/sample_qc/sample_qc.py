2890c851f6b36c9a15e14f08d50b98532d62dd8e
jkgoodrich <jgoodric@broadinstitute.org>
2021-01-20 15:49:24 -0700

    Remove unneeded TODO

--- gnomad_qc/v3/sample_qc/sample_qc.py
+++ gnomad_qc/v3/sample_qc/sample_qc.py
@@ -944,7 +944,7 @@ def main(args):
         pop_pca_eigenvalues, pop_pca_scores_ht, pop_pca_loadings_ht = run_pca(args.include_unreleasable_samples, args.n_pcs, samples_to_drop)
         pop_pca_scores_ht.write(ancestry_pca_scores(args.include_unreleasable_samples).path, overwrite=args.overwrite)
         pop_pca_loadings_ht.write(ancestry_pca_loadings(args.include_unreleasable_samples).path, overwrite=args.overwrite)
-        with hl.utils.hadoop_open(ancestry_pca_eigenvalues(args.include_unreleasable_samples), mode='w') as f:  # TODO: add this path to resources
+        with hl.utils.hadoop_open(ancestry_pca_eigenvalues(args.include_unreleasable_samples), mode='w') as f:
             f.write(",".join([str(x) for x in pop_pca_eigenvalues]))
 
     if args.assign_pops:
