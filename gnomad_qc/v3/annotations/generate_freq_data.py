12d3318cf2c8f377f34c449451c8e69e47aec571
gtiao <gtiao@broadinstitute.org>
2021-01-22 15:13:34 -0500

    Fixing pylint issue (undefined variable, meta_ht)

--- gnomad_qc/v3/annotations/generate_freq_data.py
+++ gnomad_qc/v3/annotations/generate_freq_data.py
@@ -161,6 +161,8 @@ def main(args):
             )
 
             # Compute callset-wide age histogram global
+            meta_ht = meta.ht().select('release')
+            meta_ht = met_ht.filter(meta_ht.release)
             mt = mt.annotate_globals(
                 age_distribution=meta_ht.aggregate_cols(
                     hl.agg.hist(
