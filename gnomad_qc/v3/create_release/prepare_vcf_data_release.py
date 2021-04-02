a971cbe658c796a4a42b6faa628b0a4a5f419c9a
jkgoodrich <jgoodric@broadinstitute.org>
2021-04-02 15:05:13 -0600

    Use rekey_new_reference

--- gnomad_qc/v3/create_release/prepare_vcf_data_release.py
+++ gnomad_qc/v3/create_release/prepare_vcf_data_release.py
@@ -424,17 +424,7 @@ def main(args):
             logger.info("Reading in release HT...")
             ht = hl.read_table(release_ht_path())
             export_reference = build_export_reference()
-
-            #TODO: Confirm that this is needed
-            ht = ht.rename({"locus": "locus_original"})
-            ht = ht.annotate(
-                locus=hl.locus(
-                    ht.locus_original.contig,
-                    ht.locus_original.position,
-                    reference_genome=export_reference
-                )
-            )
-            ht = ht.key_by("locus", "alleles").drop("locus_original")
+            ht = rekey_new_reference(ht, export_reference)
 
             if chromosome:
                 ht = hl.filter_intervals(
