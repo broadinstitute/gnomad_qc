ddef8257e6159bd33427330c641355a775cb01ae
jkgoodrich <jgoodric@broadinstitute.org>
2021-07-19 15:50:29 -0600

    Remove AS_VQSLOD from the info struct -- also in the vqsr struct

--- gnomad_qc/v3/create_release/create_hgdp_tgp_subset.py
+++ gnomad_qc/v3/create_release/create_hgdp_tgp_subset.py
@@ -643,6 +643,7 @@ def prepare_variant_annotations(ht: hl.Table, filter_lowqual: bool = True) -> hl
 
     logger.info("Assembling 'info' field...")
     info_fields = SITE_FIELDS + AS_FIELDS
+    info_fields.remove("AS_VQSLOD")
     missing_info_fields = set(info_fields).difference(info_ht.info.keys())
     select_info_fields = set(info_fields).intersection(info_ht.info.keys())
     logger.info(
