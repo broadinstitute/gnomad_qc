a96c94c3fa50b51f276e5c035072c209f9c36de4
jkgoodrich <jgoodric@broadinstitute.org>
2021-10-26 10:26:47 -0600

    Cleanup and add HT write

--- gnomad_qc/v3/variant_qc/compare_genome_exome_af.py
+++ gnomad_qc/v3/variant_qc/compare_genome_exome_af.py
@@ -23,6 +23,7 @@ gnomad_public_resource_configuration.source = (
 
 POPS = ["afr", "sas", "amr", "eas", "nfe"]
 POP_FORMAT = {"v2": "gnomad_{}", "v3": "{}-adj"}
+VERSIONS = ["v2", "v3"]
 
 
 def main(args):
@@ -61,36 +62,66 @@ def main(args):
     ht = ht.filter((hl.len(ht.filters_v3) == 0) & (hl.len(ht.filters_v2) == 0))
 
     ht = ht.annotate(
-        **{
-            f"n_ref_{version}_{pop}": ht[f"freq_{version}"][
-                ht[f"freq_index_dict_{version}"][POP_FORMAT[version].format(pop)]
-            ].AN
-            - ht[f"freq_{version}"][
-                ht[f"freq_index_dict_{version}"][POP_FORMAT[version].format(pop)]
-            ].AC
-            for pop in POPS
-            for version in ["v2", "v3"]
-        },
-        **{
-            f"n_alt_{version}_{pop}": ht[f"freq_{version}"][
-                ht[f"freq_index_dict_{version}"][POP_FORMAT[version].format(pop)]
-            ].AC
-            for pop in POPS
-            for version in ["v2", "v3"]
-        },
+        n_ref=hl.struct(
+            **{
+                pop: hl.struct(
+                    **{
+                        version: ht[f"freq_{version}"][
+                            ht[f"freq_index_dict_{version}"][
+                                POP_FORMAT[version].format(pop)
+                            ]
+                        ].AN
+                        - ht[f"freq_{version}"][
+                            ht[f"freq_index_dict_{version}"][
+                                POP_FORMAT[version].format(pop)
+                            ]
+                        ].AC
+                        for version in VERSIONS
+                    }
+                )
+                for pop in POPS
+            }
+        ),
+        n_alt=hl.struct(
+            **{
+                pop: hl.struct(
+                    **{
+                        version: ht[f"freq_{version}"][
+                            ht[f"freq_index_dict_{version}"][
+                                POP_FORMAT[version].format(pop)
+                            ]
+                        ].AC
+                        for version in VERSIONS
+                    }
+                )
+                for pop in POPS
+            }
+        ),
     )
 
     # Filtered to minimum AC in each population
     # min_ac = 5
 
     ht = ht.annotate(
-        **{
-            f"chi_squared_{pop}": hl.chi_squared_test(
-                ht.n_alt_v3, ht.n_ref_v3, ht.n_alt_v2, ht.n_ref_v2
-            )
-            for pop in POPS
-        }
+        chi_squared=hl.struct(
+            **{
+                pop: hl.chi_squared_test(
+                    ht.n_alt_v3, ht.n_ref_v3, ht.n_alt_v2, ht.n_ref_v2
+                )
+                for pop in POPS
+            }
+        ),
+        fisher_exact=hl.struct(
+            **{
+                pop: hl.fisher_exact_test(
+                    ht.n_alt_v3, ht.n_ref_v3, ht.n_alt_v2, ht.n_ref_v2
+                )
+                for pop in POPS
+            }
+        ),
     )
+
+    ht.write(f"gs://gnomad-julia/gnomad_v4/v2_v3_compare_freq/v2_exomes_v3_genomes.compare_freq{'.test' if args.test else ''}.ht")
 
     # Biallelic sites
 
