40e49ea3343858aeff56a8ccdc410d0eca01f24d
klaricch <kristen@broadinstitute.org>
2022-01-19 14:15:48 -0500

    add read if exists

--- gnomad_qc/v3/sample_qc/subpop_analysis.py
+++ gnomad_qc/v3/sample_qc/subpop_analysis.py
@@ -180,6 +180,7 @@ def main(args):  # noqa: D103
                 if args.test
                 else filtered_subpop_qc_mt(pop),
                 overwrite=args.overwrite,
+                _read_if_exists=not args.overwrite,
             )
 
         if not include_unreleasable_samples:
