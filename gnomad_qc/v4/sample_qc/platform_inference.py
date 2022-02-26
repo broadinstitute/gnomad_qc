413cae6d87d971d2f150104ae3ba9696fe652d37
jkgoodrich <jgoodric@broadinstitute.org>
2022-02-26 08:55:58 -0700

    Add an option for using gnomad v4 test dataset, and Help descriptions to argumments

--- gnomad_qc/v4/sample_qc/platform_inference.py
+++ gnomad_qc/v4/sample_qc/platform_inference.py
@@ -7,9 +7,18 @@ from gnomad.sample_qc.platform import (
     assign_platform_from_pcs,
     run_platform_pca,
 )
+from gnomad.utils.file_utils import file_exists
 from gnomad.utils.slack import slack_notifications
 
-from gnomad_qc.v4.resources.basics import calling_intervals, get_gnomad_v4_vds, logging_path
+from gnomad_qc.slack_creds import slack_token
+from gnomad_qc.v4.resources.basics import (
+    calling_intervals,
+    get_checkpoint_path,
+    get_gnomad_v4_vds,
+    gnomad_v4_testset,
+    gnomad_v4_testset_meta,
+    get_logging_path,
+)
 from gnomad_qc.v4.resources.sample_qc import (
     hard_filtered_samples,
     interval_coverage,
@@ -29,26 +38,52 @@ def main(args):
 
     try:
         if args.compute_coverage:
-            vds = get_gnomad_v4_vds(remove_hard_filtered_samples=False)
+            if args.test:
+                vds = gnomad_v4_testset.vds()
+            else:
+                vds = get_gnomad_v4_vds(remove_hard_filtered_samples=False).vds()
             ht = calling_intervals(
                 args.calling_interval_name, args.calling_interval_padding
             ).ht()
             mt = hl.vds.interval_coverage(vds, intervals=ht)
-            mt.write(interval_coverage.path)
+            mt.write(
+                get_checkpoint_path("test_interval_coverage", mt=True)
+                if args.test
+                else interval_coverage.path,
+                overwrite=args.overwrite,
+            )
 
         if args.run_platform_pca:
             logger.info("Running platform PCA...")
-            mt = interval_coverage.mt()
+            if args.test:
+                test_coverage_path = get_checkpoint_path(
+                    "test_interval_coverage", mt=True
+                )
+                if file_exists(test_coverage_path):
+                    mt = hl.read_matrix_table(test_coverage_path)
+                else:
+                    raise FileNotFoundError(
+                        "The test interval coverage MatrixTable does not exist. Please run --compute_coverage with the --test argument."
+                    )
+            else:
+                mt = interval_coverage.mt()
 
-            logger.info("Removing hard filtered samples from interval coverage MT...")
-            ht = hard_filtered_samples.ht()
+            logger.info(
+                "Removing hard filtered samples from interval coverage MatrixTable..."
+            )
+            if args.test:
+                ht = gnomad_v4_testset_meta.ht()
+                ht = ht.filter(ht.hard_filtered_no_sex)
+            else:
+                ht = hard_filtered_samples.ht()
+
             mt = mt.filter_cols(hl.is_defined(ht[mt.col_key]))
 
-            logger.info("Filter interval coverage MT to autosomes...")
+            logger.info("Filter interval coverage MatrixTable to autosomes...")
             mt = mt.filter_rows(mt.interval.start.in_autosome())
 
             logger.info(
-                "Annotating interval coverage MT with 'callrate' defined as fraction over dp 0..."
+                "Annotating interval coverage MatrixTable with 'callrate' defined as fraction over dp 0..."
             )
             # Default `hl.vds.interval_coverage` will return a list for `fraction_over_dp_threshold` where the second element is dp >= 1 (dp > 0)
             mt = mt.annotate_entries(callrate=mt.fraction_over_dp_threshold[1])
@@ -57,12 +92,45 @@ def main(args):
             eigenvalues, scores_ht, loadings_ht = run_platform_pca(
                 mt, binarization_threshold=None
             )
-            scores_ht.write(platform_pca_scores.path, overwrite=args.overwrite)
-            loadings_ht.write(platform_pca_loadings.path, overwrite=args.overwrite)
+            scores_ht.write(
+                get_checkpoint_path("test_platform_scores")
+                if args.test
+                else platform_pca_scores.path,
+                overwrite=args.overwrite,
+            )
+            loadings_ht.write(
+                get_checkpoint_path("test_platform_loadings")
+                if args.test
+                else platform_pca_loadings.path,
+                overwrite=args.overwrite,
+            )
+
+            eigenvalues_ht = hl.Table.parallelize(
+                hl.literal(
+                    [{"PC": i + 1, "eigenvalue": x} for i, x in enumerate(eigenvalues)],
+                    "array<struct{PC: int, eigenvalue: float}>",
+                )
+            )
+            eigenvalues_ht.write(
+                get_checkpoint_path("test_platform_eigenvalues")
+                if args.test
+                else platform_pca_eigenvalues.path,
+                overwrite=args.overwrite,
+            )
 
         if args.assign_platforms:
             logger.info("Assigning platforms based on platform PCA clustering")
-            scores_ht = hl.read_table(platform_pca_scores.ht())
+            if args.test:
+                test_scores_path = get_checkpoint_path("test_platform_scores")
+                if file_exists(test_scores_path):
+                    scores_ht = hl.read_table(test_scores_path)
+                else:
+                    raise FileNotFoundError(
+                        "The test interval coverage MatrixTable does not exist. Please run --compute_coverage with the --test argument."
+                    )
+            else:
+                scores_ht = hl.read_table(platform_pca_scores.ht())
+
             platform_ht = assign_platform_from_pcs(
                 scores_ht,
                 hdbscan_min_cluster_size=args.hdbscan_min_cluster_size,
@@ -78,38 +146,59 @@ def main(args):
             )
             platform_ht = platform_ht.repartition(args.n_partitions)
             platform_ht = platform_ht.checkpoint(
-                platform.path, overwrite=args.overwrite
+                get_checkpoint_path("test_platform_assignment")
+                if args.test
+                else platform.path,
+                overwrite=args.overwrite,
             )
             logger.info(f"Platform PCA Table count: {platform_ht.count()}")
 
     finally:
         logger.info("Copying log to logging bucket...")
-        hl.copy_log(logging_path())
+        hl.copy_log(get_logging_path())
 
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--compute_coverage", help="", action="store_true",
+        "-o",
+        "--overwrite",
+        help="Overwrite output files (default: False).",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--test",
+        help="Use the v4 test dataset instead of the full dataset.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--compute_coverage",
+        help="Compute per interval coverage metrics using Hail's vds.interval_coverage method.",
+        action="store_true",
     )
     parser.add_argument(
         "--calling_interval_name",
-        help="",
+        help="Name of calling intervals to use for interval coverage. One of: 'ukb', 'broad', or 'intersection'.",
         type=str,
         choices=["ukb", "broad", "intersection"],
+        default="intersection",
     )
     parser.add_argument(
-        "--calling_interval_padding", help="", type=int, choices=[0, 50],
+        "--calling_interval_padding",
+        help="Number of base pair padding to use on the calling intervals. One of 0 or 50 bp.",
+        type=int,
+        choices=[0, 50],
+        default=50,
     )
     parser.add_argument(
         "--run_platform_pca",
-        help="Runs platform PCA (assumes callrate MT was computed)",
+        help="Runs platform PCA (assumes coverage MatrixTable was computed, --compute_coverage).",
         action="store_true",
     )
     parser.add_argument(
         "--assign_platforms",
-        help="Assigns platforms based on callrate PCA results using HDBSCAN",
+        help="Assigns platforms based on per interval fraction of bases over DP 0 PCA results using HDBSCAN.",
         action="store_true",
     )
     parser.add_argument(
@@ -122,13 +211,7 @@ if __name__ == "__main__":
         "--hdbscan_min_cluster_size",
         help="Minimum cluster size parameter for HDBSCAN.",
         type=int,
-        default=100,
-    )
-    parser.add_argument(
-        "-o",
-        "--overwrite",
-        help="Overwrite all data from this subset (default: False)",
-        action="store_true",
+        default=50,
     )
     parser.add_argument(
         "--slack_channel", help="Slack channel to post results and notifications to."
