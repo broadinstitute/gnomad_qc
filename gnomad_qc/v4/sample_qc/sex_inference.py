541d0ee67aafa690dede915be13da6dedabebc80
jkgoodrich <jgoodric@broadinstitute.org>
2022-03-21 14:44:20 -0600

    Use LGT instead of GT (already filtered to bi-allelic)

--- gnomad_qc/v4/sample_qc/sex_inference.py
+++ gnomad_qc/v4/sample_qc/sex_inference.py
@@ -308,7 +308,7 @@ def main(args):
         mt = mt.filter_rows(hl.len(mt.alleles) == 2)
         n_samples = mt.count_cols()
         ht = mt.annotate_rows(
-            AN=hl.agg.count_where(hl.is_defined(mt.GT)) * 2,
+            AN=hl.agg.count_where(hl.is_defined(mt.LGT)) * 2,
             AC=hl.agg.sum(mt.GT.n_alt_alleles),
         ).rows()
         ht = ht.annotate(AF=ht.AC / ht.AN, callrate=ht.AN / (n_samples * 2))
