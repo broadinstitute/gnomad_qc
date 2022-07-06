ef4d87f8528487ed3522d92019d2f1139a2cabab
jkgoodrich <jgoodric@broadinstitute.org>
2022-07-06 13:07:31 -0600

    reformat

--- gnomad_qc/v4/sample_qc/platform_inference.py
+++ gnomad_qc/v4/sample_qc/platform_inference.py
@@ -31,9 +31,13 @@ logger.setLevel(logging.INFO)
 
 
 def main(args):
-    hl.init(log="/platform_pca.log", default_reference="GRCh38", tmp_dir='gs://gnomad-tmp-4day')
+    hl.init(
+        log="/platform_pca.log",
+        default_reference="GRCh38",
+        tmp_dir="gs://gnomad-tmp-4day",
+    )
     # NOTE: remove this flag when the new shuffle method is the default
-    hl._set_flags(use_new_shuffle='1')
+    hl._set_flags(use_new_shuffle="1")
 
     calling_interval_name = args.calling_interval_name
     calling_interval_padding = args.calling_interval_padding
