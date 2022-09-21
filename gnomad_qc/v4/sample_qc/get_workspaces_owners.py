5bd64793eefb7c97bdbbfc79b018a4cce5c099d6
Katherine Chao <kchao@broadinstitute.org>
2022-09-21 11:31:09 -0400

    ran black

--- gnomad_qc/v4/sample_qc/get_workspaces_owners.py
+++ gnomad_qc/v4/sample_qc/get_workspaces_owners.py
@@ -6,39 +6,46 @@ pulls owners associated with workspace, and writes workspace + owners
 to output TSV.
 """
 #!/usr/bin/env python3
-​
+
 # Install dependencies with:
 # pip install google-auth requests
 #
 # Set up Application Default Credentials with:
 # gcloud auth application-default login
-​
-​
+
 import argparse
 import csv
 import fileinput
 import sys
-​
+
 import google.auth
 from google.auth.transport.requests import AuthorizedSession
-​
-​
+
+
 parser = argparse.ArgumentParser()
-parser.add_argument("files", metavar="file", nargs="+", help="File containing workspaces (one per line, formatted namespace/name).")
+parser.add_argument(
+    "files",
+    metavar="file",
+    nargs="+",
+    help="File containing workspaces (one per line, formatted namespace/name).",
+)
 args = parser.parse_args()
-​
+
 workspaces = list(line.strip() for line in fileinput.input(args.files))
-​
+
 credentials, _ = google.auth.default()
 session = AuthorizedSession(credentials)
-​
+
 writer = csv.writer(sys.stdout, delimiter="\t")
-​
+
 for workspace in workspaces:
-    owners = session.get(
-        f"https://rawls.dsde-prod.broadinstitute.org/api/workspaces/{workspace}",
-        params={"fields": "owners"}
-    ).json().get("owners")
-    
+    owners = (
+        session.get(
+            f"https://rawls.dsde-prod.broadinstitute.org/api/workspaces/{workspace}",
+            params={"fields": "owners"},
+        )
+        .json()
+        .get("owners")
+    )
+
     writer.writerow([workspace, ",".join(owners)])
-    
