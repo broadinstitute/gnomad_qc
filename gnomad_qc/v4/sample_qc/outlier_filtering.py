8befcb67e9616904ea9f7880367922b104e358c1
jkgoodrich <jgoodric@broadinstitute.org>
2023-04-03 23:32:20 -0600

    Add use of **kwargs back

--- gnomad_qc/v4/sample_qc/outlier_filtering.py
+++ gnomad_qc/v4/sample_qc/outlier_filtering.py
@@ -124,7 +124,9 @@ def apply_filter(
 
     # Run filtering method using defined expressions, and any other passed parameters.
     if filtering_method == "stratified":
-        ht = apply_stratified_filtering_method(sample_qc_ht, qc_metrics, **ann_exprs)
+        ht = apply_stratified_filtering_method(
+            sample_qc_ht, qc_metrics, **ann_exprs, **kwargs
+        )
     elif filtering_method == "regressed":
         ht = apply_regressed_filtering_method(
             sample_qc_ht, qc_metrics, **ann_exprs, **kwargs
