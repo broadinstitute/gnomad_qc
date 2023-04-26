36f7d17a9040757bf2312109fd0589f945a78ba3
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2023-04-26 11:24:48 -0600

    Use TRUTH_SAMPLES dict correctly

--- gnomad_qc/v4/sample_qc/create_sample_qc_metadata_ht.py
+++ gnomad_qc/v4/sample_qc/create_sample_qc_metadata_ht.py
@@ -541,7 +541,7 @@ def get_sample_filter_ht(base_ht: hl.Table, relationship_ht: hl.Table) -> hl.Tab
 
     # Annotate control samples that are used in variant QC, but not included in the
     # release.
-    control_samples = hl.literal({s["s"] for s in TRUTH_SAMPLES})
+    control_samples = hl.literal({v["s"] for k, v in TRUTH_SAMPLES.items()})
     sample_filters_ht = sample_filters_ht.annotate(
         control=(control_samples.contains(sample_filters_ht.s))
     )
