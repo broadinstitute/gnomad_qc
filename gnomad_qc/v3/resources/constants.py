a36c6aa7d66276629b376c0a8cdf8b4643ec07fc
heqin <heqin@broadinstitute.org>
2023-05-18 15:59:52 -0400

    change CURRENT RELEASE to 3.1.4

--- gnomad_qc/v3/resources/constants.py
+++ gnomad_qc/v3/resources/constants.py
@@ -3,8 +3,8 @@
 VERSIONS = ["3", "3.1"]
 CURRENT_VERSION = "3.1"
 
-RELEASES = ["3.0", "3.1", "3.1.1", "3.1.2"]
-CURRENT_RELEASE = "3.1.2"
+RELEASES = ["3.0", "3.1", "3.1.1", "3.1.2", "3.1.3", "3.1.4"]
+CURRENT_RELEASE = "3.1.4"
 
 # NOTE: There were updates to the in silico annotations for v3.1.1
 INSILICO_ANNOTATION_VERSIONS = ["3.1", "3.1.1"]
