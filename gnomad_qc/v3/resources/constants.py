9610bdef372f218499a3fa0870f70056fe853327
heqin <heqin@broadinstitute.org>
2023-05-19 11:25:19 -0400

    remove 3.1.3 from RELEASES

--- gnomad_qc/v3/resources/constants.py
+++ gnomad_qc/v3/resources/constants.py
@@ -3,7 +3,9 @@
 VERSIONS = ["3", "3.1"]
 CURRENT_VERSION = "3.1"
 
-RELEASES = ["3.0", "3.1", "3.1.1", "3.1.2", "3.1.3", "3.1.4"]
+# NOTE: There was no v3.1.3 release for the sites HT and VCFs, it
+# consisted of only an STR release.
+RELEASES = ["3.0", "3.1", "3.1.1", "3.1.2", "3.1.4"]
 CURRENT_RELEASE = "3.1.4"
 
 # NOTE: There were updates to the in silico annotations for v3.1.1
