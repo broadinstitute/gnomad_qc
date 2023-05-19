3fa556ad9ca6bfe651b33cdc4b0140b1737dd27b
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2023-05-19 09:49:54 -0600

    A couple fixes to use `CURRENT_VERSION` instead of `CURRENT_RELEASE` that didn't get updated when we changed it

--- gnomad_qc/v3/resources/meta.py
+++ gnomad_qc/v3/resources/meta.py
@@ -8,33 +8,28 @@ from gnomad.resources.resource_utils import (
     VersionedTableResource,
 )
 
-from gnomad_qc.v3.resources.constants import CURRENT_RELEASE, CURRENT_VERSION, RELEASES
+from gnomad_qc.v3.resources.constants import CURRENT_VERSION, VERSIONS
 
 
 # Samples metadata
-def _meta_root_path(version: str = CURRENT_RELEASE) -> str:
+def _meta_root_path(version: str = CURRENT_VERSION) -> str:
     """
     Retrieve the path to the root metadata directory.
 
-    :param version: gnomAD release version
+    :param version: gnomAD version
     :return: String representation of the path to the root metadata directory
     """
     return f"gs://gnomad/metadata/genomes_v{version}"
 
 
-def meta_tsv_path(
-    version: str = CURRENT_RELEASE, meta_version: str = CURRENT_VERSION
-) -> str:
+def meta_tsv_path(version: str = CURRENT_VERSION) -> str:
     """
     Get the path to the finalized sample metadata information after sample QC.
 
-    :param version: gnomAD release version
-    :param meta_version: metadata version to return
+    :param version: gnomAD version
     :return: String path to the finalized metadata
     """
-    return (
-        f"{_meta_root_path(version)}/gnomad_v{version}_metadata_v{meta_version}.tsv.gz"
-    )
+    return f"{_meta_root_path(version)}/gnomad_v{version}_metadata.tsv.gz"
 
 
 _meta_versions = {
@@ -101,11 +96,11 @@ project_meta = VersionedTableResource(CURRENT_VERSION, _project_meta_versions)
 pedigree = VersionedPedigreeResource("3.1", _pedigree_versions)
 trios = VersionedPedigreeResource("3.1", _trios_versions)
 ped_mendel_errors = VersionedTableResource(
-    CURRENT_RELEASE,
+    CURRENT_VERSION,
     {
-        release: TableResource(
-            path=f"{_meta_root_path(release)}/gnomad_v{release}_ped_chr20_mendel_errors.ht"
+        version: TableResource(
+            path=f"{_meta_root_path(version)}/gnomad_v{version}_ped_chr20_mendel_errors.ht"
         )
-        for release in RELEASES
+        for version in VERSIONS
     },
 )
