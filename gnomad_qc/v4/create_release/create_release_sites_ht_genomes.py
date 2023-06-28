33d1d8ea6e43918c506ffd005b01091be47b2c6c
heqin <heqin@broadinstitute.org>
2023-06-28 16:12:11 -0400

    re-add replace_oth_with_remaining

--- gnomad_qc/v4/create_release/create_release_sites_ht_genomes.py
+++ gnomad_qc/v4/create_release/create_release_sites_ht_genomes.py
@@ -26,3 +26,33 @@ def remove_missing_vep_fields(vep_expr: hl.StructExpression) -> hl.StructExpress
             **{consequence: vep_expr[consequence].map(lambda x: x.drop("minimised"))}
         )
     return vep_expr
+
+
+def replace_oth_with_remaining(ht: hl.Table) -> hl.Table:
+    """
+    Replace 'oth' with 'remaining' in Global fields of a Table.
+
+    .. note::
+        This function is used to rename ancestry group fields to match v4 exomes.
+        Which means that all "oth" will be changed to "remaining".
+        2 Global fields are renamed: freq_meta (in the value of the key "pop") and freq_index_dict (in the keys).
+
+    :param ht: release sites Table to be modified
+    :return: release sites Table with 'oth' replaced with 'remaining'
+    """
+    # in freq_meta, type: array<dict<str, str>>, the value of key "pop" is
+    # changed from 'oth' to 'remaining'.
+    ht = ht.annotate_globals(
+        freq_meta=ht.freq_meta.map(
+            lambda d: d.map_values(lambda x: x.replace("oth", "remaining"))
+        )
+    )
+
+    # in freq_index_dict, type: dict<str, int>, the keys containing 'oth'
+    # inside are changed to 'remaining'.
+    oldkeys = hl.eval(ht.freq_index_dict.keys())
+    newkeys = [s.replace("oth", "remaining") for s in oldkeys]
+    vals = hl.eval(ht.freq_index_dict.values())
+    freq_index_dict_new = {k: v for k, v in zip(newkeys, vals)}
+    ht = ht.annotate_globals(freq_index_dict=freq_index_dict_new)
+    return ht
