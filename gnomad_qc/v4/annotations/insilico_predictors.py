a2518ffbc986ec228c1cf96b484e3f959573111b
Qin He <44242118+KoalaQin@users.noreply.github.com>
2023-07-24 12:56:12 -0400

    address Mike's suggestions

--- gnomad_qc/v4/annotations/insilico_predictors.py
+++ gnomad_qc/v4/annotations/insilico_predictors.py
@@ -20,17 +20,20 @@ def create_cadd_grch38_ht() -> hl.Table:
     """
     Create a Hail Table with CADD scores for GRCh38.
 
-    .. note:: The CADD scores are from the following sources:
+    The combined CADD scores in the returned table are from the following sources:
         - all SNVs: `cadd.v1.6.whole_genome_SNVs.tsv.bgz` (81G) downloaded from `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz`. It contains 8,812,917,339 SNVs.
-        - gnomad 3.0 indels: `cadd.v1.6.indels.tsv.bgz` (1.1G) downloaded from `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/gnomad.genomes.r3.0.indel.tsv.gz`. It contains 100,546,109 indels in gnomaD v3.0.
-        - gnomad 3.1 indels: `CADD-indels-gnomad.3.1.ht` was run by William Phu with CADD v1.6 in 2020. It contains 166,122,720 indels in gnomAD v3.1.
-        - gnomad 3.1 complex indels: `CADD-1.6-gnomad-complex-variants.ht` was run by William Phu with CADD v1.6 in 2020. It contains 2,307 complex variants that were not filtered out by Hail is.indels().
-        - gnomAD v4 indels: `cadd.v1.6.gnomAD_v4_new_indels.tsv.bgz` (368M) was run by Qin He with CADD v1.6 in 2023. It contains 32,561,253 indel that are new in gnomAD v4.
-        - 1,972,208 indels were duplicated in gnomAD v3.0 and v4.0, in gnomAD v3.1 and v4.0. However, CADD only generate a score per loci. We use anti_join to keep only the latest prediction.
+        - gnomad 3.0 indels: `cadd.v1.6.indels.tsv.bgz` (1.1G) downloaded from `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/gnomad.genomes.r3.0.indel.tsv.gz`. It contains 100,546,109 indels from gnomaD v3.0.
+        - gnomad 3.1 indels: `CADD-indels-gnomad.3.1.ht` was run on gnomAD v3.1 with CADD v1.6 in 2020. It contains 166,122,720 indels from gnomAD v3.1.
+        - gnomad 3.1 complex indels: `CADD-1.6-gnomad-complex-variants.ht` was run on gnomAD v3.1 with CADD v1.6 in 2020. It contains 2,307 complex variants that do not fit Hail's criteria for an indel and thus exist in a separate table than the gnomad 3.1 indels.
+        - gnomAD v4 indels: `cadd.v1.6.gnomAD_v4_new_indels.tsv.bgz` (368M) was run on gnomAD v4 with CADD v1.6 in 2023. It contains 32,561,253 indels that are new in gnomAD v4.
+
+         .. note::
+         1,972,208 indels were duplicated in gnomAD v3.0 and v4.0 or in gnomAD v3.1 and v4.0. However, CADD only generates a score per loci. We keep only the latest prediction, v4.0, for these loci.
     :return: Hail Table with CADD scores for GRCh38.
     """
 
-    def load_cadd_raw(cadd_tsv) -> hl.Table:
+    def _load_cadd_raw(cadd_tsv) -> hl.Table:
+        """Functions to load CADD raw data in TSV format to Hail Table."""
         column_names = {
             "f0": "chr",
             "f1": "pos",
@@ -42,13 +45,11 @@ def create_cadd_grch38_ht() -> hl.Table:
         types = {"f0": hl.tstr, "f1": hl.tint32, "f4": hl.tfloat32, "f5": hl.tfloat32}
         ht = hl.import_table(
             cadd_tsv,
-            delimiter="\t",
             types=types,
             no_header=True,
             force_bgz=True,
             comment="#",
             min_partitions=1000,
-            missing="NA",
         )
         ht = ht.rename(column_names)
         chr = hl.if_else(ht.chr.startswith("chr"), ht.chr, hl.format("chr%s", ht.chr))
@@ -61,17 +62,17 @@ def create_cadd_grch38_ht() -> hl.Table:
 
         return ht
 
-    snvs = load_cadd_raw(
+    snvs = _load_cadd_raw(
         "gs://gnomad-qin/v4_annotations/cadd.v1.6.whole_genome_SNVs.tsv.bgz"
     )
-    indel3_0 = load_cadd_raw(
+    indel3_0 = _load_cadd_raw(
         "gs://gnomad-qin/v4_annotations/cadd.v1.6.gnomad.genomes.r3.0.indel.tsv.bgz"
     )
     indel3_1 = hl.read_table("gs://gnomad-wphu/CADD-indels-gnomad.3.1.ht")
     indel3_1_complex = hl.read_table(
         "gs://gnomad-wphu/CADD-1.6-gnomad-complex-variants.ht"
     )
-    indel4 = load_cadd_raw(
+    indel4 = _load_cadd_raw(
         "gs://gnomad-qin/v4_annotations/cadd.v1.6.gnomAD_v4_new_indels.tsv.bgz"
     )
 
@@ -82,6 +83,7 @@ def create_cadd_grch38_ht() -> hl.Table:
     indel3 = indel3.anti_join(indel4)
 
     ht = snvs.union(indel3, indel4)
+    ht = ht.annotate_globals(cadd_version="v1.6")
     return ht
 
 
