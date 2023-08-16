c73b940a3bb1730fb9c3d76ba1d3c6d8269007c2
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2023-08-16 16:21:39 -0600

    Fix import spacing

--- gnomad_qc/v4/annotations/generate_variant_qc_annotations.py
+++ gnomad_qc/v4/annotations/generate_variant_qc_annotations.py
@@ -4,6 +4,7 @@ import argparse
 import logging
 
 import hail as hl
+
 from gnomad.assessment.validity_checks import count_vep_annotated_variants_per_interval
 from gnomad.resources.grch38.reference_data import ensembl_interval
 from gnomad.sample_qc.relatedness import filter_mt_to_trios
