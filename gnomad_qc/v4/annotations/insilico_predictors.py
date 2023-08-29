b2ca391cef195c422112b5a03a63a502e1d3b800
Qin He <44242118+KoalaQin@users.noreply.github.com>
2023-08-29 17:20:45 -0400

    address review comments

--- gnomad_qc/v4/annotations/insilico_predictors.py
+++ gnomad_qc/v4/annotations/insilico_predictors.py
@@ -22,14 +22,26 @@ def create_cadd_grch38_ht() -> hl.Table:
     Create a Hail Table with CADD scores for GRCh38.
 
     The combined CADD scores in the returned table are from the following sources:
-        - all SNVs: `cadd.v1.6.whole_genome_SNVs.tsv.bgz` (81G) downloaded from `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz`. It contains 8,812,917,339 SNVs.
-        - gnomad 3.0 indels: `cadd.v1.6.indels.tsv.bgz` (1.1G) downloaded from `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/gnomad.genomes.r3.0.indel.tsv.gz`. It contains 100,546,109 indels from gnomaD v3.0.
-        - gnomad 3.1 indels: `CADD-indels-gnomad.3.1.ht` was run on gnomAD v3.1 with CADD v1.6 in 2020. It contains 166,122,720 indels from gnomAD v3.1.
-        - gnomad 3.1 complex indels: `CADD-1.6-gnomad-complex-variants.ht` was run on gnomAD v3.1 with CADD v1.6 in 2020. It contains 2,307 complex variants that do not fit Hail's criteria for an indel and thus exist in a separate table than the gnomad 3.1 indels.
-        - gnomAD v4 indels: `cadd.v1.6.gnomAD_v4_new_indels.tsv.bgz` (368M) was run on gnomAD v4 with CADD v1.6 in 2023. It contains 32,561,253 indels that are new in gnomAD v4.
+        - all SNVs: `cadd.v1.6.whole_genome_SNVs.tsv.bgz` (81G) downloaded from
+        `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz`.
+        It contains 8,812,917,339 SNVs.
+        - gnomad 3.0 indels: `cadd.v1.6.indels.tsv.bgz` (1.1G) downloaded from `
+        https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/gnomad.genomes.r3.0.indel.tsv.gz`.
+        It contains 100,546,109 indels from gnomaD v3.0.
+        - gnomad 3.1 indels: `CADD-indels-gnomad.3.1.ht` was run on gnomAD v3.1
+        with CADD v1.6 in 2020. It contains 166,122,720 indels from gnomAD v3.1.
+        - gnomad 3.1 complex indels: `CADD-1.6-gnomad-complex-variants.ht` was
+        run on gnomAD v3.1 with CADD v1.6 in 2020. It contains 2,307 complex
+        variants that do not fit Hail's criteria for an indel and thus exist in
+        a separate table than the gnomad 3.1 indels.
+        - gnomAD v4 indels: `cadd.v1.6.gnomAD_v4_new_indels.tsv.bgz` (368M) was
+        run on gnomAD v4 with CADD v1.6 in 2023. It contains 32,561,253 indels
+        that are new in gnomAD v4.
 
          .. note::
-         1,972,208 indels were duplicated in gnomAD v3.0 and v4.0 or in gnomAD v3.1 and v4.0. However, CADD only generates a score per loci. We keep only the latest prediction, v4.0, for these loci.
+         1,972,208 indels were duplicated in gnomAD v3.0 and v4.0 or in gnomAD
+         v3.1 and v4.0. However, CADD only generates a score per loci.
+         We keep only the latest prediction, v4.0, for these loci.
     :return: Hail Table with CADD scores for GRCh38.
     """
 
@@ -88,18 +100,24 @@ def create_cadd_grch38_ht() -> hl.Table:
     return ht
 
 
-def create_revel_grch38_ht(revel_csv, ensembl_ids) -> hl.Table:
+def create_revel_grch38_ht(revel_csv: str, ensembl_ids: str) -> hl.Table:
     """
     Create a Hail Table with REVEL scores for GRCh38.
 
-    .. Note::
-    Starting from gnomAD v4, we will use REVEL scores for MANE Select transcripts and canonical transcripts only.
-    We take a max of the REVEL scores if a variant falls on multiple transcripts.
-    Since the Ensembl ID used by REVEL was not from Ensembl 105, we also only filtered transcripts that are in Ensembl 105.
-    This will also deprecate `has_duplicate` field as in gnomAD v3.
+    .. note::
+    Starting with gnomAD v4, we use REVEL scores for only MANE Select and
+    canonical transcripts. Even when a variant falls on multiple MANE/canonical
+    transcripts of different genes, the scores are equal.
+    REVEL's Ensembl ID is not from Ensembl 105, so we filter to transcripts
+    that are in Ensembl 105.
+    This deprecates the `has_duplicate` field present in gnomAD v3.
 
-    :param revel_csv: Path to REVEL CSV file.
-    :param ensembl_ids: Path to Ensembl 105 ID file, downloaded from Ensembl 105 archive.
+    :param revel_csv: Temporary path to REVEL CSV file, downloaded from
+    https://www.google.com/url?q=https%3A%2F%2Frothsj06.dmz.hpc.mssm.edu%2Frevel-v1.3_all_chromosomes.zip&sa=D&sntz=1&usg=AOvVaw2DS2TWUYl__0vqijzzxp5M
+    size ~526M, ~82,100,677 variants
+    :param ensembl_ids: temporary path to Ensembl 105 ID file,
+    downloaded from Ensembl 105 archive. It must contain the following columns:
+    Transcript stable ID, Ensembl Canonical, MANE Select
     :return: Hail Table with REVEL scores for GRCh38.
     """
     ht = hl.import_table(
@@ -108,13 +126,10 @@ def create_revel_grch38_ht(revel_csv, ensembl_ids) -> hl.Table:
         min_partitions=1000,
         types={"grch38_pos": hl.tstr, "REVEL": hl.tfloat64},
     )
-    ht = ht.checkpoint(
-        "gs://gnomad-tmp-4day/revel-v1.3_all_chromosomes_with_transcript_ids.ht",
-        _read_if_exists=True,
-    )
 
     logger.info("Annotating REVEL table...")
     ht = ht.drop("hg19_pos", "aaref", "aaalt")
+    # drop variants that have no position in GRCh38 when lifted over from GRCh37
     ht = ht.filter(ht.grch38_pos.contains("."), keep=False)
     ht = ht.transmute(chr="chr" + ht.chr)
     ht = ht.annotate(
@@ -127,20 +142,17 @@ def create_revel_grch38_ht(revel_csv, ensembl_ids) -> hl.Table:
     ht = ht.key_by("Transcript_stable_ID")
 
     logger.info("Import and process the ensembl ID file...")
-    ensembl_ids = hl.import_table(ensembl_ids, min_partitions=200, impute=True)
+    ensembl_ids = hl.import_table(
+        ensembl_ids, min_partitions=200, impute=True, key="Transcript_stable_ID"
+    )
     ensembl_ids = ensembl_ids.select(
         "Transcript_stable_ID", "Ensembl_Canonical", "MANE_Select"
     )
-    ensembl_ids = ensembl_ids.key_by("Transcript_stable_ID")
-
-    ht = ht.annotate(
-        in_Ensembl105=hl.is_defined(ensembl_ids[ht.Transcript_stable_ID]),
-        MANE_Select=ensembl_ids[ht.Transcript_stable_ID].MANE_Select,
-        Ensembl_Canonical=ensembl_ids[ht.Transcript_stable_ID].Ensembl_Canonical,
-    )
+    logger.info("Annotating revel HT with canonical and MANE Select transcripts...")
+    ht = ht.annotate(**ensembl_ids[ht.Transcript_stable_ID])
 
     logger.info("Filtering transcripts that are in Ensembl 105...")
-    ht = ht.filter(hl.is_defined(ht.in_Ensembl105), keep=True)
+    ht = ht.filter(ht.in_Ensembl105, keep=True)
 
     logger.info(
         "Annotating REVEL scores for MANE Select transcripts and canonical"
@@ -148,13 +160,9 @@ def create_revel_grch38_ht(revel_csv, ensembl_ids) -> hl.Table:
     )
     ht = ht.key_by("locus", "alleles")
     ht = ht.annotate(
-        revel_mane=hl.if_else(ht.MANE_Select != "", ht.REVEL, hl.missing(hl.tfloat)),
-        revel_canonical=hl.if_else(
-            ht.Ensembl_Canonical == "1", ht.REVEL, hl.missing(hl.tfloat)
-        ),
-        revel_noncanonical=hl.if_else(
-            ht.Ensembl_Canonical != "1", ht.REVEL, hl.missing(hl.tfloat)
-        ),
+        revel_mane=hl.or_missing(ht.MANE_Select != "", ht.REVEL),
+        revel_canonical=hl.or_missing(ht.Ensembl_Canonical == "1", ht.REVEL),
+        revel_noncanonical=hl.or_missing(ht.Ensembl_Canonical != "1", ht.REVEL),
     )
 
     logger.info("Taking max REVEL scores for MANE Select transcripts...")
@@ -163,13 +171,8 @@ def create_revel_grch38_ht(revel_csv, ensembl_ids) -> hl.Table:
     )
     mane_ht = ht.filter(hl.is_defined(ht.revel_mane), keep=True)
     max_revel_mane = mane_ht.group_by(*mane_ht.key).aggregate(
-        max_revel=hl.agg.max(mane_ht.revel_mane)
-    )
-    transcripts_mane = mane_ht.group_by(*mane_ht.key).aggregate(
-        transcripts=hl.agg.collect_as_set(mane_ht.Transcript_stable_ID)
-    )
-    max_revel_mane = max_revel_mane.annotate(
-        transcripts=transcripts_mane[max_revel_mane.key].transcripts
+        max_revel=hl.agg.max(mane_ht.revel_mane),
+        transcripts=hl.agg.collect_as_set(mane_ht.Transcript_stable_ID),
     )
 
     logger.info("Taking max REVEL scores for canonical transcripts...")
@@ -177,15 +180,9 @@ def create_revel_grch38_ht(revel_csv, ensembl_ids) -> hl.Table:
         (~hl.is_defined(ht.revel_mane)) & (hl.is_defined(ht.revel_canonical)), keep=True
     )
     max_revel_canonical = canonical_ht.group_by(*canonical_ht.key).aggregate(
-        max_revel=hl.agg.max(canonical_ht.revel_canonical)
+        max_revel=hl.agg.max(canonical_ht.revel_canonical),
+        transcripts=hl.agg.collect_as_set(canonical_ht.Transcript_stable_ID),
     )
-    transcripts_canonical = canonical_ht.group_by(*canonical_ht.key).aggregate(
-        transcripts=hl.agg.collect_as_set(canonical_ht.Transcript_stable_ID)
-    )
-    max_revel_canonical = max_revel_canonical.annotate(
-        transcripts=transcripts_canonical[max_revel_canonical.key].transcripts
-    )
-
     logger.info(
         "Merge max REVEL scores for MANE Select transcripts and canonical transcripts"
         " to one HT..."
@@ -220,7 +217,6 @@ def main(args):
             "gs://gnomad-qin/ensembl/ensembl105_chr1-22XY_MANE_canonical_ucscid.tsv.bgz"
         )
 
-        create_revel_grch38_ht(revel_csv, ensembl_ids)
         ht = create_revel_grch38_ht(revel_csv, ensembl_ids)
         ht.write(
             get_insilico_predictors(predictor="revel").path,
