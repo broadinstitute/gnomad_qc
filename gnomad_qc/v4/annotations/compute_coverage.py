06f558da2c067dca954be0081ab8fb225c4b8f96
klaricch <kristen@broadinstitute.org>
2023-09-01 14:09:16 -0400

    v4 coverage

--- gnomad_qc/v4/annotations/compute_coverage.py
+++ gnomad_qc/v4/annotations/compute_coverage.py
@@ -6,138 +6,19 @@ from typing import List, Union
 
 import hail as hl
 from gnomad.resources.grch38.gnomad import (
-    CURRENT_GENOME_COVERAGE_RELEASE,
+    CURRENT_EXOME_COVERAGE_RELEASE,
     coverage,
     coverage_tsv_path,
 )
-from gnomad.resources.grch38.reference_data import (
-    telomeres_and_centromeres,
-    vep_context,
-)
+from gnomad.resources.grch38.reference_data import vep_context
 from gnomad.utils.sparse_mt import compute_coverage_stats
 
-from gnomad_qc.v4.resources.basics import get_gnomad_v4_vds
+from gnomad_qc.resource_utils import check_resource_existence
+from gnomad_qc.v4.resources.basics import calling_intervals, get_gnomad_v4_vds
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger("coverage")
 logger.setLevel(logging.INFO)
-
-
-def compute_coverage_stats(
-    mtds: Union[hl.MatrixTable, hl.vds.VariantDataset],
-    reference_ht: hl.Table,
-    coverage_over_x_bins: List[int] = [1, 5, 10, 15, 20, 25, 30, 50, 100],
-) -> hl.Table:
-    """
-    Compute coverage statistics for every base of the `reference_ht` provided.
-
-    The following coverage stats are calculated:
-        - mean
-        - median
-        - total DP
-        - fraction of samples with coverage above X, for each x in `coverage_over_x_bins`
-
-    The `reference_ht` is a table that contains row for each locus coverage should be computed on.
-    It needs to be keyed with the same keys as `mt`, typically either `locus` or `locus, alleles`.
-    The `reference_ht` can e.g. be created using `get_reference_ht`
-
-    :param mtds: Input sparse MT
-    :param reference_ht: Input reference HT
-    :param coverage_over_x_bins: List of boundaries for computing samples over X
-    :return: Table with per-base coverage stats
-    """
-    is_vds = isinstance(mtds, hl.vds.VariantDataset)
-    if is_vds:
-        n_samples = mtds.variant_data.count_cols()
-    else:
-        n_samples = mtds.count_cols()
-
-    print(f"Computing coverage stats on {n_samples} samples.")
-
-    # Create an outer join with the reference Table
-    def join_with_ref(mt: hl.MatrixTable) -> hl.MatrixTable:
-        keep_entries = ["DP"]
-        if "END" in mt.entry:
-            keep_entries.append("END")
-        mt.select_entries(*keep_entries).select_cols().select_rows()
-        col_key_fields = list(mt.col_key)
-        t = mt._localize_entries("__entries", "__cols")
-        t = t.join(reference_ht.key_by(*mt.row_key).select(_in_ref=True), how="outer")
-        t = t.annotate(
-            __entries=hl.or_else(
-                t.__entries,
-                hl.range(n_samples).map(
-                    lambda x: hl.missing(t.__entries.dtype.element_type)
-                ),
-            )
-        )
-        return t._unlocalize_entries("__entries", "__cols", col_key_fields)
-
-    if is_vds:
-        rmt = mtds.reference_data
-        vmt = mtds.variant_data
-        mtds = hl.vds.VariantDataset(join_with_ref(rmt), join_with_ref(vmt))
-        mt = hl.vds.to_dense_mt(mtds)
-    else:
-        mtds = join_with_ref(mtds)
-        # Densify
-        mt = hl.experimental.densify(mtds)
-
-    # Filter rows where the reference is missing
-    mt = mt.filter_rows(mt._in_ref)
-
-    # Unfilter entries so that entries with no ref block overlap aren't null
-    mt = mt.unfilter_entries()
-
-    # Compute coverage stats
-    coverage_over_x_bins = sorted(coverage_over_x_bins)
-    max_coverage_bin = coverage_over_x_bins[-1]
-    hl_coverage_over_x_bins = hl.array(coverage_over_x_bins)
-
-    # This expression creates a counter DP -> number of samples for DP between
-    # 0 and max_coverage_bin
-    coverage_counter_expr = hl.agg.counter(
-        hl.min(max_coverage_bin, hl.or_else(mt.DP, 0))
-    )
-
-    # This expression aggregates the DP counter in reverse order of the coverage_over_x_bins
-    # and computes the cumulative sum over them.
-    # It needs to be in reverse order because we want the sum over samples
-    # covered by > X.
-    count_array_expr = hl.cumulative_sum(
-        hl.array(
-            [
-                hl.int32(coverage_counter_expr.get(max_coverage_bin, 0))
-            ]  # The coverage was already floored to the max_coverage_bin, so no more aggregation is needed for the max bin
-            # For each of the other bins, coverage needs to be summed between the
-            # boundaries
-        ).extend(
-            hl.range(hl.len(hl_coverage_over_x_bins) - 1, 0, step=-1).map(
-                lambda i: hl.sum(
-                    hl.range(
-                        hl_coverage_over_x_bins[i - 1], hl_coverage_over_x_bins[i]
-                    ).map(lambda j: hl.int32(coverage_counter_expr.get(j, 0)))
-                )
-            )
-        )
-    )
-    mean_expr = hl.agg.mean(hl.or_else(mt.DP, 0))
-
-    # Annotate rows now
-    return mt.select_rows(
-        mean=hl.if_else(hl.is_nan(mean_expr), 0, mean_expr),
-        median_approx=hl.or_else(hl.agg.approx_median(hl.or_else(mt.DP, 0)), 0),
-        total_DP=hl.agg.sum(mt.DP),
-        **{
-            f"over_{x}": count_array_expr[i] / n_samples
-            for i, x in zip(
-                range(
-                    len(coverage_over_x_bins) - 1, -1, -1
-                ),  # Reverse the bin index as count_array_expr has the reverse order
-                coverage_over_x_bins,
-            )
-        },
-    ).rows()
 
 
 def main(args):  # noqa: D103
@@ -146,59 +27,117 @@ def main(args):  # noqa: D103
         default_reference="GRCh38",
         tmp_dir="gs://gnomad-tmp-4day",
     )
-    # TODO: Determine coverage version from args
-    coverage_version = (
-        args.coverage_version
-        if args.coverage_version
-        else CURRENT_GENOME_COVERAGE_RELEASE
-    )
-    test = args.test
 
-    if args.compute_coverage_ht:
-        # TODO: See if the context HT looks OK and use that one. Might need to remove
-        #  telomeres and centromeres.
-        ref_ht = vep_context.versions["105"].ht()
-        if test:
-            ref_ht = ref_ht._filter_partitions(range(50))
-
-        vds = get_gnomad_v4_vds(
-            release_only=True, filter_partitions=range(2) if test else None
+    try:
+        coverage_version = (
+            args.coverage_version
+            if args.coverage_version
+            else CURRENT_EXOME_COVERAGE_RELEASE
         )
+        test = args.test
 
-        coverage_ht = compute_coverage_stats(vds, ref_ht)
+        if args.compute_coverage_ht:
+            check_resource_existence(
+                output_step_resources={
+                    "--compute-coverage-ht": [
+                        coverage("exomes").versions[coverage_version].path
+                    ],
+                },
+                overwrite=args.overwrite,
+            )
 
-        coverage_ht = coverage_ht.checkpoint(
-            "gs://gnomad-tmp/gnomad.genomes_v3.coverage.summary.ht", overwrite=True
-        )
-        coverage_ht.describe()
-        coverage_ht.show()
-        # coverage_ht = coverage_ht.naive_coalesce(5000)
+            logger.info("Running compute coverage...")
+            # Read in context Table
+            ref_ht = vep_context.versions["105"].ht()
+            if test:
+                ref_ht = ref_ht._filter_partitions(range(50))
 
-        # coverage_ht.write(
-        #    coverage("genomes").versions[coverage_version].path,
-        #    overwrite=args.overwrite,
-        # )
+            # Read in calling intervals
+            interval_ht = calling_intervals(
+                interval_name=args.calling_interval_name,
+                calling_interval_padding=args.calling_interval_padding,
+            ).ht()
+
+            # Read in VDS
+            vds = get_gnomad_v4_vds(
+                release_only=True,
+                test=test,
+                filter_partitions=range(2) if test else None,
+            )
+
+            # Compute coverage stats
+            coverage_ht = compute_coverage_stats(vds, ref_ht, interval_ht)
+
+            # Checkpoint Table
+            coverage_ht = coverage_ht.checkpoint(
+                "gs://gnomad-tmp/gnomad.genomes_v4.coverage.summary.ht",
+                overwrite=args.overwrite,
+            )
+
+            # Write final result if not testing
+            if not test:
+                coverage_ht = coverage_ht.naive_coalesce(5000)
+
+                coverage_ht.write(
+                    coverage("exomes").versions[coverage_version].path,
+                    overwrite=args.overwrite,
+                )
+
+        if args.export_coverage_tsv:
+            logger.info("Exporting coverage tsv...")
+            ht = coverage("exomes").versions[coverage_version].ht()
+            ht.export(coverage_tsv_path("exomes", coverage_version))
+
+    finally:
+        hl.copy_log(f"gs://gnomad-tmp-4day/coverage/compute_coverage.log")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--overwrite", help="Overwrites existing hail tables", action="store_true"
+        "--overwrite", help="Overwrite existing hail Tables.", action="store_true"
     )
     parser.add_argument(
         "--test",
-        help="",
+        help=(
+            "Whether to run a test using only the first 2 partitions of the VDS test"
+            " dataset."
+        ),
         action="store_true",
     )
     parser.add_argument(
-        "--compute_coverage_ht", help="Computes the coverage HT", action="store_true"
+        "--compute-coverage-ht", help="Compute the coverage HT.", action="store_true"
     )
     parser.add_argument(
-        "--coverage_version",
+        "--coverage-version",
+        type=str,
         help=(
             "Specifies coverage version to read/write. If not set,"
-            " gnomad.resources.grch38.gnomad.CURRENT_GENOME_COVERAGE_RELEASE is used."
+            " gnomad.resources.grch38.gnomad.CURRENT_EXOME_COVERAGE_RELEASE is used."
         ),
+    )
+    parser.add_argument(
+        "--calling-interval-name",
+        help=(
+            "Name of calling intervals to use for interval coverage. One of: 'ukb',"
+            " 'broad', 'intersection', or 'union'."
+        ),
+        type=str,
+        choices=["ukb", "broad", "intersection", "union"],
+        default="union",
+    )
+    parser.add_argument(
+        "--calling-interval-padding",
+        help=(
+            "Number of base pair padding to use on the calling intervals. One of 0 or"
+            " 50 bp."
+        ),
+        type=int,
+        choices=[0, 50],
+        default=50,
+    )
+    parser.add_argument(
+        "--export-coverage-tsv", help="Exports coverage TSV file.", action="store_true"
     )
 
     main(parser.parse_args())
