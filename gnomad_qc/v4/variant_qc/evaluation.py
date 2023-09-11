cb997bd87a49d6f37680ce0a3139d5ba9a2ba827
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2023-09-10 22:15:13 -0600

    Changes for v4

--- gnomad_qc/v4/variant_qc/evaluation.py
+++ gnomad_qc/v4/variant_qc/evaluation.py
@@ -4,7 +4,7 @@ import logging
 from pprint import pformat
 
 import hail as hl
-from gnomad.resources.grch38.reference_data import clinvar, telomeres_and_centromeres
+from gnomad.resources.grch38.reference_data import clinvar
 from gnomad.utils.filtering import filter_low_conf_regions, filter_to_clinvar_pathogenic
 from gnomad.utils.slack import slack_notifications
 from gnomad.variant_qc.evaluation import (
@@ -20,83 +20,60 @@ from gnomad_qc.resource_utils import (
     PipelineStepResourceCollection,
 )
 from gnomad_qc.slack_creds import slack_token
-from gnomad_qc.v3.resources import (
-    allele_data,
-    fam_stats,
-    get_binned_concordance,
-    get_checkpoint_path,
+from gnomad_qc.v4.resources.annotations import (
     get_info,
-    get_rf_annotations,
+    get_trio_stats,
+    get_variant_qc_annotations,
+)
+from gnomad_qc.v4.resources.basics import TRUTH_SAMPLES_S, get_gnomad_v4_vds
+from gnomad_qc.v4.resources.variant_qc import (
+    TRUTH_SAMPLES,
+    get_binned_concordance,
+    get_callset_truth_data,
     get_rf_result,
     get_score_bins,
-    get_vqsr_filters,
 )
-from gnomad_qc.v4.resources.annotations import get_info
-from gnomad_qc.v4.resources.basics import get_gnomad_v4_vds
-from gnomad_qc.v4.resources.variant_qc import TRUTH_SAMPLES, get_callset_truth_data
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger("variant_qc_evaluation")
 logger.setLevel(logging.INFO)
 
 
-def create_bin_ht(model_id: str, n_bins: int, hgdp_tgp_subset: bool) -> hl.Table:
+def create_bin_ht(
+    ht: hl.Table,
+    info_ht: hl.Table,
+    rf_annotations_ht: hl.Table,
+    n_bins: int,
+    vqsr: bool = False,
+) -> hl.Table:
     """
     Create a table with bin annotations added for a RF or VQSR run and writes it to its correct location in annotations.
 
-    :param model_id: Which variant QC model (RF or VQSR model ID) to annotate with bin
-    :param n_bins: Number of bins to bin the data into
-    :param hgdp_tgp_subset: Whether this is for the HGDP + 1KG/TGP subset, if True this will filter the HT using the subset's raw allele count instead of gnomAD's high quality raw allele count
-    :return: Table with bin annotations
+    :param n_bins: Number of bins to bin the data into.
+    :return: Table with bin annotations.
     """
-    logger.info(f"Annotating {model_id} HT with bins using {n_bins} bins")
-    info_ht = get_info(split=True).ht()
-    if model_id.startswith("vqsr"):
-        rf_ht = get_rf_annotations().ht()
-        ht = get_vqsr_filters(model_id, split=True).ht()
-
+    if vqsr:
         ht = ht.annotate(
-            **rf_ht[ht.key],
+            **rf_annotations_ht[ht.key],
             info=info_ht[ht.key].info,
             score=ht.info.AS_VQSLOD,
             positive_train_site=ht.info.POSITIVE_TRAIN_SITE,
             negative_train_site=ht.info.NEGATIVE_TRAIN_SITE,
             AS_culprit=ht.info.AS_culprit,
         )
-
-        if hgdp_tgp_subset:
-            allele_data_ht = allele_data.ht()
-            ht = ht.annotate(**allele_data_ht[ht.key].allele_data)
-        else:
-            # Remove all variants with an undefined ac_raw because ac_raw was calculated on the high quality samples only
-            # and VQSR was run before sample filtering
-            ht = ht.filter(hl.is_defined(ht.ac_raw))
-
     else:
-        if hgdp_tgp_subset:
-            raise ValueError(
-                "The hgdp_tgp_subset option is only relevant for VQSR models in all v3"
-                " releases."
-            )
-
-        ht = get_rf_result(model_id=model_id).ht()
         ht = ht.annotate(
+            score=ht.rf_probability["TP"],
             info=info_ht[ht.key].info,
             positive_train_site=ht.tp,
             negative_train_site=ht.fp,
-            score=ht.rf_probability["TP"],
         )
 
-    ht = ht.filter(
-        ~info_ht[ht.key].AS_lowqual
-        & ~hl.is_defined(telomeres_and_centromeres.ht()[ht.locus])
-    )
+    ht = ht.filter(~info_ht[ht.key].AS_lowqual)
     ht_non_lcr = filter_low_conf_regions(
         ht,
-        filter_lcr=True,
-        # TODO: Uncomment when we have decoy path
-        filter_decoy=False,  # True,
-        filter_segdup=True,
+        filter_decoy=False,
+        filter_telomeres_and_centromeres=True,
     )
     ht = ht.annotate(non_lcr=hl.is_defined(ht_non_lcr[ht.key]))
     bin_ht = create_binned_ht(ht, n_bins, add_substrat={"non_lcr": ht.non_lcr})
@@ -104,7 +81,30 @@ def create_bin_ht(model_id: str, n_bins: int, hgdp_tgp_subset: bool) -> hl.Table
     return bin_ht
 
 
-def create_aggregated_bin_ht(model_id: str) -> hl.Table:
+def score_bin_validity_check(ht: hl.Table) -> None:
+    """
+    Check that the bin scoring looks correct by printing so aggregate numbers.
+
+    :param ht: Table with bin annotations.
+    :return: None
+    """
+    print(
+        ht.aggregate(
+            hl.struct(
+                was_biallelic=hl.agg.counter(~ht.was_split),
+                has_biallelic_rank=hl.agg.counter(hl.is_defined(ht.biallelic_bin)),
+                was_singleton=hl.agg.counter(ht.singleton),
+                has_singleton_rank=hl.agg.counter(hl.is_defined(ht.singleton_bin)),
+                was_biallelic_singleton=hl.agg.counter(ht.singleton & ~ht.was_split),
+                has_biallelic_singleton_rank=hl.agg.counter(
+                    hl.is_defined(ht.biallelic_singleton_bin)
+                ),
+            )
+        )
+    )
+
+
+def create_aggregated_bin_ht(ht: hl.Table, trio_stats_ht: hl.Table) -> hl.Table:
     """
     Aggregate variants into bins using previously annotated bin information.
 
@@ -117,12 +117,10 @@ def create_aggregated_bin_ht(model_id: str) -> hl.Table:
 
     For each bin, aggregates statistics needed for evaluation plots.
 
-    :param str model_id: Which variant QC model (RF or VQSR model ID) to group
-    :return: Table of aggregate statistics by bin
+    :param ht: Table with bin annotations.
+    :return: Table of aggregate statistics by bin.
     """
-    ht = get_score_bins(model_id, aggregated=False).ht()
-
-    # Count variants for ranking
+    # Count variants for ranking.
     count_expr = {
         x: hl.agg.filter(
             hl.is_defined(ht[x]),
@@ -134,18 +132,17 @@ def create_aggregated_bin_ht(model_id: str) -> hl.Table:
         if x.endswith("bin")
     }
     bin_variant_counts = ht.aggregate(hl.struct(**count_expr))
-    logger.info(f"Found the following variant counts:\n {pformat(bin_variant_counts)}")
     ht = ht.annotate_globals(bin_variant_counts=bin_variant_counts)
+    logger.info(f"Found the following variant counts:\n {pformat(bin_variant_counts)}")
 
-    # Load ClinVar pathogenic data
+    # Load ClinVar pathogenic data.
     clinvar_pathogenic_ht = filter_to_clinvar_pathogenic(clinvar.ht())
     ht = ht.annotate(clinvar_path=hl.is_defined(clinvar_pathogenic_ht[ht.key]))
-    trio_stats_ht = fam_stats.ht()
 
     logger.info(f"Creating grouped bin table...")
     grouped_binned_ht = compute_grouped_binned_ht(
         ht,
-        checkpoint_path=get_checkpoint_path(f"grouped_bin_{model_id}"),
+        checkpoint_path=new_temp_file(f"grouped_bin", "ht"),
     )
 
     logger.info(f"Aggregating grouped bin table...")
@@ -161,12 +158,14 @@ def create_aggregated_bin_ht(model_id: str) -> hl.Table:
 def get_evaluation_resources(
     test: bool,
     overwrite: bool,
+    model_id: str = None,
 ) -> PipelineResourceCollection:
     """
     Get PipelineResourceCollection for all resources needed in the variant QC evaluation pipeline.
 
     :param test: Whether to gather all resources for testing.
     :param overwrite: Whether to overwrite resources if they exist.
+    :param model_id: Model ID of RF model results to use.
     :return: PipelineResourceCollection containing resources for all steps of the
         variant QC evaluation pipeline.
     """
@@ -181,6 +180,37 @@ def get_evaluation_resources(
         },
     )
 
+    # Create resource collection for each step of the variant QC evaluation pipeline.
+    create_bin_table = PipelineStepResourceCollection(
+        "--create-bin-ht",
+        input_resources={
+            "annotations/generate_variant_qc_annotations.py --create-variant-qc-annotation-ht": {
+                "rf_ht": get_variant_qc_annotations()
+            },
+            "random_forest.py --apply-rf": {
+                "rf_result_ht": get_rf_result(model_id=model_id, test=test)
+            },
+        },
+        output_resources={
+            "bin_ht": get_score_bins(model_id, aggregated=False, test=test),
+        },
+    )
+    validity_check = PipelineStepResourceCollection(
+        "--score-bin-validity-check",
+        pipeline_input_steps=[create_bin_table],
+    )
+    create_aggregated_bin_table = PipelineStepResourceCollection(
+        "--create-aggregated-bin-ht",
+        output_resources={
+            "agg_bin_ht": get_score_bins(model_id, aggregated=True, test=test)
+        },
+        pipeline_input_steps=[create_bin_table],
+        add_input_resources={
+            "annotations/generate_variant_qc_annotations.py --generate-trio-stats": {
+                "trio_stats_ht": get_trio_stats()
+            },
+        },
+    )
     extract_truth_samples = PipelineStepResourceCollection(
         "--extract-truth-samples",
         output_resources={
@@ -204,12 +234,24 @@ def get_evaluation_resources(
             for s in TRUTH_SAMPLES
         },
     )
+    bin_truth_sample_concordance = PipelineStepResourceCollection(
+        "--bin-truth-sample-concordance",
+        pipeline_input_steps=[create_bin_table, merge_with_truth_data],
+        output_resources={
+            f"{s}_bin_ht": get_binned_concordance(model_id, s, test=test)
+            for s in TRUTH_SAMPLES_S
+        },
+    )
 
     # Add all steps to the variant QC evaluation pipeline resource collection.
     evaluation_pipeline.add_steps(
         {
+            "create_bin_table": create_bin_table,
+            "validity_check": validity_check,
+            "create_aggregated_bin_table": create_aggregated_bin_table,
             "extract_truth_samples": extract_truth_samples,
             "merge_with_truth_data": merge_with_truth_data,
+            "bin_truth_sample_concordance": bin_truth_sample_concordance,
         }
     )
 
@@ -226,47 +268,33 @@ def main(args):
 
     overwrite = args.overwrite
     test = args.test
+    model_id = args.model_id
+    n_bins = args.n_bins
 
     evaluation_resources = get_evaluation_resources(
         test=test,
         overwrite=overwrite,
+        model_id=model_id,
     )
     info_ht = evaluation_resources.info_ht.ht()
 
     if args.create_bin_ht:
-        create_bin_ht(args.model_id, args.n_bins, args.hgdp_tgp_subset).write(
-            get_score_bins(
-                args.model_id, aggregated=False, hgdp_tgp_subset=args.hgdp_tgp_subset
-            ).path,
-            overwrite=args.overwrite,
-        )
+        logger.info(f"Annotating {model_id} HT with bins using {n_bins} bins...")
+        res = evaluation_resources.create_bin_table
+        res.check_resource_existence()
+        ht = create_bin_ht(res.rf_result_ht.ht(), info_ht, res.rf_ht.ht(), n_bins)
+        ht.write(res.bin_ht.path, overwrite=overwrite)
 
-    if args.run_sanity_checks:
-        ht = get_score_bins(args.model_id, aggregated=False).ht()
-        logger.info("Running sanity checks...")
-        print(
-            ht.aggregate(
-                hl.struct(
-                    was_biallelic=hl.agg.counter(~ht.was_split),
-                    has_biallelic_rank=hl.agg.counter(hl.is_defined(ht.biallelic_bin)),
-                    was_singleton=hl.agg.counter(ht.singleton),
-                    has_singleton_rank=hl.agg.counter(hl.is_defined(ht.singleton_bin)),
-                    was_biallelic_singleton=hl.agg.counter(
-                        ht.singleton & ~ht.was_split
-                    ),
-                    has_biallelic_singleton_rank=hl.agg.counter(
-                        hl.is_defined(ht.biallelic_singleton_bin)
-                    ),
-                )
-            )
-        )
+    if args.score_bin_validity_check:
+        logger.info("Running validity checks on score bins Table...")
+        score_bin_validity_check(res.bin_ht.ht())
 
     if args.create_aggregated_bin_ht:
         logger.warning("Use only workers, it typically crashes with preemptibles")
-        create_aggregated_bin_ht(args.model_id).write(
-            get_score_bins(args.model_id, aggregated=True).path,
-            overwrite=args.overwrite,
-        )
+        res = evaluation_resources.create_aggregated_bin_table
+        res.check_resource_existence()
+        ht = create_aggregated_bin_ht(res.bin_ht.ht(), res.trio_stats_ht.ht())
+        ht.write(res.agg_bin_ht.path, overwrite=overwrite)
 
     if args.extract_truth_samples:
         logger.info(f"Extracting truth samples from VDS...")
@@ -309,48 +337,32 @@ def main(args):
             ht.write(getattr(res, f"{ts}_ht").path, overwrite=overwrite)
 
     if args.bin_truth_sample_concordance:
-        for truth_sample in TRUTH_SAMPLES:
+        res = evaluation_resources.bin_truth_sample_concordance
+        res.check_resource_existence()
+        for s in TRUTH_SAMPLES_S:
             logger.info(
-                f"Creating binned concordance table for {truth_sample} for model"
-                f" {args.model_id}"
+                f"Creating binned concordance table for {s} for model {model_id}..."
             )
-            ht = get_callset_truth_data(truth_sample, mt=False).ht()
-
-            info_ht = get_info(split=True).ht()
-            ht = ht.filter(
-                ~info_ht[ht.key].AS_lowqual
-                & ~hl.is_defined(telomeres_and_centromeres.ht()[ht.locus])
-            )
+            metric_ht = res.bin_ht.ht()
+            ht = getattr(res, f"{s}_ht").ht()
+            ht = ht.annotate(score=metric_ht[ht.key].score)
 
             logger.info("Filtering out low confidence regions and segdups...")
             ht = filter_low_conf_regions(
                 ht,
-                filter_lcr=True,
-                # TODO: Uncomment when we have decoy path
-                filter_decoy=False,  # True,
-                filter_segdup=True,
+                filter_decoy=False,
+                filter_telomeres_and_centromeres=True,
             )
 
-            logger.info(
-                "Loading HT containing RF or VQSR scores annotated with a bin based on"
-                " the rank of score..."
-            )
-            metric_ht = get_score_bins(args.model_id, aggregated=False).ht()
-            ht = ht.filter(hl.is_defined(metric_ht[ht.key]))
-
-            ht = ht.annotate(score=metric_ht[ht.key].score)
-
-            ht = compute_binned_truth_sample_concordance(ht, metric_ht, args.n_bins)
-            ht.write(
-                get_binned_concordance(args.model_id, truth_sample).path,
-                overwrite=args.overwrite,
-            )
+            ht = ht.filter(hl.is_defined(ht.score) & ~info_ht[ht.key].AS_lowqual)
+            ht = compute_binned_truth_sample_concordance(ht, metric_ht, n_bins)
+            ht.write(getattr(res, f"{s}_bin_ht").path, overwrite=overwrite)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--slack_channel", help="Slack channel to post results and notifications to."
+        "--slack-channel", help="Slack channel to post results and notifications to."
     )
     parser.add_argument(
         "--overwrite",
@@ -358,35 +370,24 @@ if __name__ == "__main__":
         action="store_true",
     )
     parser.add_argument(
-        "--model_id",
+        "--model-id",
         help="Model ID.",
         required=False,
     )
     parser.add_argument(
-        "--hgdp_tgp_subset",
-        help=(
-            "Use HGDP + 1KG/TGP subset raw allele count as a filter in `create_bin_ht`"
-            " instead of the full gnomAD raw allele count. Only relevant to VQSR"
-            " because VQSR was used for filtering in all v3 releases. This is"
-            " neededonly for the HGDP + 1KG/TGP subset release and is not used in"
-            " evaluation."
-        ),
-        action="store_true",
-    )
-    parser.add_argument(
-        "--create_bin_ht",
+        "--create-bin-ht",
         help=(
             "When set, creates file annotated with bin based on rank of VQSR/RF score."
         ),
         action="store_true",
     )
     parser.add_argument(
-        "--run_sanity_checks",
-        help="When set, runs ranking sanity checks.",
+        "--score-bin-validity-check",
+        help="When set, runs ranking validity checks.",
         action="store_true",
     )
     parser.add_argument(
-        "--create_aggregated_bin_ht",
+        "--create-aggregated-bin-ht",
         help=(
             "When set, creates a file with aggregate counts of variants based on bins."
         ),
@@ -415,10 +416,10 @@ if __name__ == "__main__":
         action="store_true",
     )
     parser.add_argument(
-        "--bin_truth_sample_concordance",
+        "--bin-truth-sample-concordance",
         help=(
             "Merges concordance results (callset vs. truth) for a given truth sample"
-            " with bins from specified model"
+            " with bins from specified model."
         ),
         action="store_true",
     )
