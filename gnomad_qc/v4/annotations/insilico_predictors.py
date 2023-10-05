adbcdec6444db9747e9b21a855b87cb4338cabc5
Qin He <44242118+KoalaQin@users.noreply.github.com>
2023-10-05 15:55:42 -0400

    update CADD input file names

--- gnomad_qc/v4/annotations/insilico_predictors.py
+++ gnomad_qc/v4/annotations/insilico_predictors.py
@@ -54,20 +54,21 @@ def create_cadd_grch38_ht() -> hl.Table:
 
     The combined CADD scores in the returned table are from the following sources:
         - all SNVs: `cadd.v1.6.whole_genome_SNVs.tsv.bgz` (81G) downloaded from
-        `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz`.
-        It contains 8,812,917,339 SNVs.
-        - gnomad 3.0 indels: `cadd.v1.6.indels.tsv.bgz` (1.1G) downloaded from
-        `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/gnomad.genomes.r3.0.indel.tsv.gz`.
-        It contains 100,546,109 indels from gnomaD v3.0.
-        - gnomad 3.1 indels: `CADD-indels-gnomad.3.1.ht` was run on gnomAD v3.1
-        with CADD v1.6 in 2020. It contains 166,122,720 indels from gnomAD v3.1.
+          `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/whole_genome_SNVs.tsv.gz`.
+          It contains 8,812,917,339 SNVs.
+        - gnomad 3.0 indels: `cadd.v1.6.gnomad.genomes.v3.0.indel.tsv.bgz` (1.1G)
+          downloaded from `https://krishna.gs.washington.edu/download/CADD/v1.6/GRCh38/gnomad.genomes.r3.0.indel.tsv.gz`.
+          It contains 100,546,109 indels from gnomaD v3.0.
+        - gnomad 3.1 indels: `cadd.v1.6.gnomad.genomes.v3.1.indels.complex.ht` was run
+          on gnomAD v3.1 with CADD v1.6 in 2020. It contains 166,122,720 indels from
+          gnomAD v3.1.
         - gnomad 3.1 complex indels: `CADD-1.6-gnomad-complex-variants.ht` was
-        run on gnomAD v3.1 with CADD v1.6 in 2020. It contains 2,307 complex
-        variants that do not fit Hail's criteria for an indel and thus exist in
-        a separate table than the gnomad 3.1 indels.
-        - gnomAD v4 indels: `cadd.v1.6.gnomAD_v4_new_indels.tsv.bgz` (368M) was
-        run on gnomAD v4 with CADD v1.6 in 2023. It contains 32,561,253 indels
-        that are new in gnomAD v4.
+          run on gnomAD v3.1 with CADD v1.6 in 2020. It contains 2,307 complex
+          variants that do not fit Hail's criteria for an indel and thus exist in
+          a separate table than the gnomad 3.1 indels.
+        - gnomAD v4 indels: `cadd.v1.6.gnomad.v4.0.indels.new.tsv.bgz` (368M) was
+          run on gnomAD v4 with CADD v1.6 in 2023. It contains 32,561,253 indels
+          that are new in gnomAD v4.
 
          .. note::
          1,972,208 indels were duplicated in gnomAD v3.0 and v4.0 or in gnomAD
@@ -110,9 +111,11 @@ def create_cadd_grch38_ht() -> hl.Table:
         "gs://gnomad-insilico/cadd/cadd.v1.6.whole_genome_SNVs.tsv.bgz"
     )
     indel3_0 = _load_cadd_raw(
-        "gs://gnomad-insilico/cadd/cadd.v1.6.gnomad.genomes.r3.0.indel.tsv.bgz"
+        "gs://gnomad-insilico/cadd/cadd.v1.6.gnomad.genomes.v3.0.indel.tsv.bgz"
     )
-    indel3_1 = hl.read_table("gs://gnomad-insilico/cadd/CADD-indels-gnomad.3.1.ht")
+    indel3_1 = hl.read_table(
+        "gs://gnomad-insilico/cadd/cadd.v1.6.gnomad.genomes.v3.1.indels.new.ht"
+    )
     indel3_1_complex = hl.read_table(
         "gs://gnomad-insilico/cadd/cadd.v1.6.gnomad.genomes.v3.1.indels.complex.ht"
     )
