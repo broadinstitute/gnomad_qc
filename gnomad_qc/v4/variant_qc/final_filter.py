a53dc51f3ee1476af0a51310d0220a2b8edd765e
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2023-10-24 09:48:05 -0600

    Change monoallelic and het flags to only flag variants that are not AC0

--- gnomad_qc/v4/variant_qc/final_filter.py
+++ gnomad_qc/v4/variant_qc/final_filter.py
@@ -330,12 +330,11 @@ def generate_final_filter_ht(
 
     # Generate expressions for mono-allelic and only-het status if requested.
     if mono_allelic_flag:
-        vqc_expr = vqc_expr.annotate(
-            monoallelic=(raw_freq_expr.AF == 1) | (raw_freq_expr.AF == 0)
-        )
+        vqc_expr = vqc_expr.annotate(monoallelic=(raw_freq_expr.AF == 1))
     if only_het_flag:
         vqc_expr = vqc_expr.annotate(
-            only_het=((adj_freq_expr.AC * 2) == adj_freq_expr.AN)
+            only_het=(raw_freq_expr.AC > 0)
+            & ((adj_freq_expr.AC * 2) == adj_freq_expr.AN)
             & (adj_freq_expr.homozygote_count == 0)
         )
 
