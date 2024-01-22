033b8bb14b17a7644f6e692921de7464052dcc1b
Daniel Marten <marten@broadinstitute.org>
2024-01-22 14:46:44 -0500

    Black, isort, formatting

--- gnomad_qc/v4/create_release/create_false_dup_liftover.py
+++ gnomad_qc/v4/create_release/create_false_dup_liftover.py
@@ -1,22 +1,18 @@
-" Script to create custom liftover file for genes CBS, KCNE1, and CRYAA"
+"Script to create custom liftover file for genes CBS, KCNE1, and CRYAA."
 import argparse
 import logging
 
 import hail as hl
 
 from gnomad_qc.v2.resources.basics import get_gnomad_liftover_data_path
-from gnomad_qc.v4.resources.constants import CURRENT_RELEASE, RELEASES
-from gnomad_qc.v4.resources.release import (
-    _release_root,
-    release_sites,
-)
-
+from gnomad_qc.v4.resources.constants import CURRENT_RELEASE
+from gnomad_qc.v4.resources.release import _release_root
 
 logging.basicConfig(
     format="%(asctime)s (%(name)s %(lineno)s): %(message)s",
     datefmt="%m/%d/%Y %I:%M:%S %p",
 )
-logger = logging.getLogger("subset")
+logger = logging.getLogger("false_dup_genes")
 logger.setLevel(logging.INFO)
 
 FALSE_DUP_GENES = ["KCNE1", "CBS", "CRYAA"]
@@ -51,7 +47,7 @@ def read_and_filter(
     )
 
     # Filter to chr21, since it is known that is where all 3 of the genes are.
-    ht = ht.filter(ht.locus.contig=="chr21")
+    ht = ht.filter(ht.locus.contig == "chr21")
 
     # Filter using lambda statement for any of the 3 genes of interest in the gene_symbol array.
     ht = ht.filter(
@@ -67,35 +63,40 @@ def read_and_filter(
 
 def main(args):
     """
-    These are the three clinically relevant genes impacted by false duplications in the GRCh38 reference. 
-    To make it easier for our users to switch to gnomAD v4 and the new reference build, 
-    we should create a new release file that combines information from the v2 exomes and genomes only for these three genes. 
-    To create this file, filter the v2 liftover files (https://gnomad.broadinstitute.org/downloads#v2-liftover) to these genes, 
+    These are the three clinically relevant genes impacted by false duplications in the GRCh38 reference.
+    To make it easier for our users to switch to gnomAD v4 and the new reference build,
+    we should create a new release file that combines information from the v2 exomes and genomes only for these three genes.
+    To create this file, filter the v2 liftover files (https://gnomad.broadinstitute.org/downloads#v2-liftover) to these genes,
     and then merge frequency information across the exomes and genomes
     """
     # Read in liftover files.
-    exome_ht = read_and_filter('exomes')
-    genome_ht = read_and_filter('genomes')
+    exome_ht = read_and_filter("exomes")
+    genome_ht = read_and_filter("genomes")
 
     # Union and merge to contain any variant present in either.
     # Note: only about ~550 overlap, with ~187k exome variants and ~12k genome variants.
     # This makes sense in exonic regions.
-    ht = exome_ht.select().select_globals().union(genome_ht.select().select_globals()).distinct()
+    ht = (
+        exome_ht.select()
+        .select_globals()
+        .union(genome_ht.select().select_globals())
+        .distinct()
+    )
 
     # Annotate with information from both exomes and genomes - many will be NaN.
     ht = ht.annotate(
-        v2_exomes = exome_ht[ht.locus,ht.alleles],
-        v2_genomes = genome_ht[ht.locus,ht.alleles],
-        v2_freq = hl.struct(
-            exome_freq = exome_ht[ht.locus,ht.alleles].freq,
-            genome_freq = genome_ht[ht.locus,ht.alleles].freq,
-        )
+        v2_exomes=exome_ht[ht.locus, ht.alleles],
+        v2_genomes=genome_ht[ht.locus, ht.alleles],
+        v2_freq=hl.struct(
+            exome_freq=exome_ht[ht.locus, ht.alleles].freq,
+            genome_freq=genome_ht[ht.locus, ht.alleles].freq,
+        ),
     )
 
     # Annotate with globals from v2 exomes and v2 genomes.
     ht = ht.annotate_globals(
-        v2_exome_globals = exome_ht.globals.collect(),
-        v2_genome_globals = genome_ht.globals.collect()
+        v2_exome_globals=exome_ht.globals.collect(),
+        v2_genome_globals=genome_ht.globals.collect(),
     )
 
     # Write output to created resource.
