b5e9363b0f13c86791439fd6a66f957cd5638175
KoalaQin <heqin@broadinstitute.org>
2024-03-22 15:11:56 -0400

    Apply review suggestions

--- gnomad_qc/v4/plots/sample_growth.R
+++ gnomad_qc/v4/plots/sample_growth.R
@@ -12,8 +12,8 @@ color_afr = k_afr = '#941494'
 color_sas = k_sas = '#FF9912'
 color_eas = k_eas = '#108C44'
 color_oth = k_oth = '#ABB9B9'
-color_mde = k_mde = '#000080'
-color_asj = k_asj = 'gold2'
+color_mde = k_mde = '#33CC33'
+color_asj = k_asj = '#FF7F50'
 
 exac.colors <- c(color_eas, color_sas, color_eur, color_mde, color_afr, color_amr, color_asj, color_oth)
 
@@ -28,7 +28,7 @@ gnomadv4.pop <- c(19850, 43129, 556006+26710, 2884, 16740, 22362, 13068, 30198)
 pdf('fordist_gnomad_datasets_prev4_sizeorder_2023_09_20.pdf', height = 5, width=8)
 barplot(as.matrix(cbind(exac.pop, gnomadv3.pop, gnomadv2.pop)),
         col=exac.colors,
-        names.arg=c("ExAC", "gnomADv3", "gnomADv2"),
+        names.arg=c("ExAC", "gnomAD v3", "gnomAD v2"),
         border=NA, beside=FALSE,  main="", axes=FALSE, ylim=c(0,145000))
 axis(side=2, at=seq(0,140000,10000), labels=formatC(seq(0,140000,10000),big.mark=',',format='fg'), las=2, cex.axis=1, lwd.ticks=1, lwd=0)
 par(las=0)
