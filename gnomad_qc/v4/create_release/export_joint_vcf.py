49c50fc1d0573f7a1ce85c21db38be7bc5be28a6
KoalaQin <heqin@broadinstitute.org>
2024-03-30 13:17:15 -0400

    add check vcf fields matching with header

--- gnomad_qc/v4/create_release/export_joint_vcf.py
+++ gnomad_qc/v4/create_release/export_joint_vcf.py
@@ -3,24 +3,28 @@ import argparse
 import logging
 from copy import deepcopy
 from pprint import pprint
-from typing import Dict, List, Optional, Tuple
+from typing import Dict, List, Optional
 
 import hail as hl
+from gnomad.resources.grch38.gnomad import POPS
 from gnomad.sample_qc.ancestry import POP_NAMES
 from gnomad.utils.release import make_freq_index_dict_from_meta
 from gnomad.utils.vcf import (
+    FAF_POPS,
     HISTS,
     REGION_FLAGS_JOINT_INFO_DICT,
     SEXES,
     adjust_vcf_incompatible_types,
+    build_vcf_export_reference,
     create_label_groups,
     make_hist_bin_edges_expr,
     make_hist_dict,
     make_info_dict,
+    rekey_new_reference,
 )
 
-from gnomad_qc.v4.create_release.validate_and_export_vcf import FAF_POPS, POPS
-from gnomad_qc.v4.resources.release import release_sites
+from gnomad_qc.v4.resources.basics import qc_temp_prefix
+from gnomad_qc.v4.resources.release import release_sites, release_vcf_path
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger("export_joint_vcf")
@@ -48,6 +52,9 @@ VCF_INFO_REORDER = [
     "fafmax_faf95_max_gen_anc",
 ]
 
+# Exomes and genomes use the same pops for v4
+POPS = deepcopy(POPS["v4"])
+
 
 def prepare_info_fields(ht: hl.Table) -> hl.expr.StructExpression:
     """
@@ -58,6 +65,10 @@ def prepare_info_fields(ht: hl.Table) -> hl.expr.StructExpression:
         expressions and updated entries
     """
     expr_dict = {}
+
+    logger.info("Unfurling region flags...")
+    for f in REGION_FLAG_FIELDS:
+        expr_dict[f] = ht.region_flags[f]
 
     for data_type in ["exomes", "genomes", "joint"]:
         for annotation in ["freq", "faf"]:
@@ -92,8 +103,8 @@ def prepare_info_fields(ht: hl.Table) -> hl.expr.StructExpression:
 
         logger.info(f"Unfurling age hists from {data_type}...")
         age_hist_idx = ht[data_type].histograms.age_hists
-        AGE_HISTS = ["age_hist_het", "age_hist_hom"]
-        for hist in AGE_HISTS:
+        age_hists = ["age_hist_het", "age_hist_hom"]
+        for hist in age_hists:
             for f in age_hist_idx[hist].keys():
                 key = f"{hist}_{f}_{data_type}"
                 expr = (
@@ -127,10 +138,6 @@ def prepare_info_fields(ht: hl.Table) -> hl.expr.StructExpression:
                     },
                 )
 
-    logger.info("Unfurling region flags...")
-    for f in REGION_FLAG_FIELDS:
-        expr_dict[f] = ht.region_flags[f]
-
     logger.info(
         "Unfurling contingency table test results of exomes and genomes freq"
         " comparison..."
@@ -155,32 +162,43 @@ def prepare_info_fields(ht: hl.Table) -> hl.expr.StructExpression:
 
 def prepare_vcf_header_dict(
     ht: hl.Table,
-    pops: Dict[str, str] = POPS,
+    pops: List[str] = POPS,
+    faf_pops: Dict[str, List[str]] = FAF_POPS,
     sexes: List[str] = SEXES,
 ) -> Dict[str, Dict[str, str]]:
     """
     Prepare VCF header dictionary.
 
     :param ht: Input Table
+    :param pops: Dict of sample global genetic ancestry names for the gnomAD data type.
+        Default is POPS.
+    :param faf_pops: Dict with gnomAD version (keys) and faf genentic ancestry group
+        names (values). Default is FAF_POPS.
+    :param sexes: gnomAD sample sexes used in VCF export. Default is SEXES.
     :return: Prepared VCF header dictionary
     """
     vcf_info_dict = {}
     vcf_info_dict.update(REGION_FLAGS_JOINT_INFO_DICT)
+    pop_names = {
+        pop: POP_NAMES[pop] if pop != "remaining" else "Remaining individuals"
+        for pop in POPS
+    }
     for data_type in ["exomes", "genomes", "joint"]:
         description_text = f" in {data_type} subset"  # TODO: do we call it subset?
         logger.info("Preparing freq VCF header for %s subset...", data_type)
         if data_type == "exomes":
-            pops_to_use = pops.copy()
+            pops_to_use = pops
+            pop_names = pop_names.copy()
         else:
-            pops_to_use = pops.copy()
-            pops_to_use.update({"ami": "Amish"})
+            pops_to_use = pops + ["ami"]
+            pop_names.update({"ami": "Amish"})
         freq_label_groups = create_label_groups(pops=pops_to_use, sexes=sexes)
         for label_group in freq_label_groups:
             vcf_info_dict.update(
                 make_info_dict(
                     prefix=data_type,
                     prefix_before_metric=False,
-                    pop_names=pops_to_use,
+                    pop_names=pop_names,
                     label_groups=label_group,
                     description_text=description_text,
                     callstats=True,
@@ -191,13 +209,14 @@ def prepare_vcf_header_dict(
             faf_pops_version = "v4"
         else:
             faf_pops_version = "v3"
-        faf_pops = {pop: POP_NAMES[pop] for pop in FAF_POPS[faf_pops_version]}
+        faf_pops = faf_pops[faf_pops_version]
+        faf_pop_names = {pop: POP_NAMES[pop] for pop in faf_pops}
         if data_type == "exomes" or data_type == "genomes":
-            # There are no sex-specific FAFs in the joint dataset.
             faf_label_groups = create_label_groups(
                 pops=faf_pops, sexes=[], all_groups=["adj"]
             )
         else:
+            # There are sex-specific FAFs in the joint dataset.
             faf_label_groups = create_label_groups(
                 pops=faf_pops, sexes=SEXES, all_groups=["adj"]
             )
@@ -206,7 +225,7 @@ def prepare_vcf_header_dict(
                 make_info_dict(
                     prefix=data_type,
                     prefix_before_metric=False,
-                    pop_names=faf_pops,
+                    pop_names=faf_pop_names,
                     label_groups=label_group,
                     faf=True,
                     description_text=description_text,
@@ -218,7 +237,7 @@ def prepare_vcf_header_dict(
         vcf_info_dict.update(
             make_info_dict(
                 suffix=data_type,
-                pop_names=pops,
+                pop_names=pop_names,
                 grpmax=True,
                 description_text=description_text,
             )
@@ -257,8 +276,7 @@ def prepare_vcf_header_dict(
             )
         )
 
-    pops_ctt = pops.copy()
-    pops_ctt.update({"ami": "Amish"})
+    pops_ctt = pops + ["ami"]
     ctt_label_groups = create_label_groups(pops=pops_ctt, sexes=sexes)
     for label_group in ctt_label_groups:
         vcf_info_dict.update(
@@ -285,9 +303,8 @@ def prepare_vcf_header_dict(
 
 def prepare_ht_for_export(
     ht: hl.Table,
-    vcf_info_reorder: Optional[List[str]] = VCF_INFO_REORDER,
     info_fields_to_drop: Optional[List[str]] = None,
-) -> Tuple[hl.Table, List[str]]:
+) -> hl.Table:
     """
     Format validated HT for export.
 
@@ -295,17 +312,9 @@ def prepare_ht_for_export(
     are VCF compatible.
 
     :param ht: Input joint release HT.
-    :param vcf_info_reorder: Order of VCF INFO fields. These will be placed in front of
-        all other fields in the order specified.
     :param info_fields_to_drop: List of info fields to drop from the info struct.
-    :return: Formatted HT and list rename row annotations.
+    :return: Formatted HT.
     """
-    if vcf_info_reorder:
-        logger.info("Rearranging fields to desired order...")
-        ht = ht.annotate(
-            info=ht.info.select(*vcf_info_reorder, *ht.info.drop(*vcf_info_reorder))
-        )
-
     # Select relevant fields for VCF export
     ht = ht.select("info")
 
@@ -340,7 +349,47 @@ def prepare_ht_for_export(
     # pipe delimited.
     ht = adjust_vcf_incompatible_types(ht, pipe_delimited_annotations=[])
 
-    return ht, new_row_annots
+    return ht
+
+
+def check_vcf_field(
+    ht: hl.Table,
+    header_dict: Dict[str, Dict[str, str]],
+) -> bool:
+    """
+    Check that all VCF fields and descriptions are present in input Table and VCF header dictionary.
+
+    :param ht: Input Table to be exported to VCF.
+    :param header_dict: VCF header dictionary.
+    :return: Boolean with whether all expected fields and descriptions are present.
+    """
+    missing_fields = []
+    missing_descriptions = []
+    annots = list(ht.info)
+
+    for field in annots:
+        try:
+            description = header_dict["info"][field]
+            if len(description) == 0:
+                logger.warning(
+                    "%s in info field has empty description in VCF header!", field
+                )
+                missing_descriptions.append(field)
+        except KeyError:
+            (logger.warning("%s in info field does not exist in VCF header!", field))
+            missing_fields.append(field)
+
+    if len(missing_fields) != 0 or len(missing_descriptions) != 0:
+        logger.error(
+            "Some fields are either missing or missing descriptions in the VCF header!"
+            " Please reconcile."
+        )
+        logger.error("Missing fields: %s", missing_fields)
+        logger.error("Missing descriptions: %s", missing_descriptions)
+        return False
+
+    logger.info("Passed VCF fields check!")
+    return True
 
 
 def main(args):
@@ -354,8 +403,9 @@ def main(args):
     overwrite = args.overwrite
     test = args.test
     contig = args.contig
+    data_type = "joint"
 
-    ht = release_sites(data_type="joint").ht()
+    ht = release_sites(data_type=data_type).ht()
 
     if contig and test:
         raise ValueError(
@@ -378,3 +428,59 @@ def main(args):
         ht = hl.filter_intervals(
             ht, [hl.parse_locus_interval(contig, reference_genome="GRCh38")]
         )
+    logger.info("Preparing info fields...")
+    ht = ht.annotate(info=prepare_info_fields(ht))
+    logger.info("Preparing VCF header dictionary...")
+    header_dict = prepare_vcf_header_dict(ht)
+    logger.info("Preparing HT for export...")
+    ht = prepare_ht_for_export(ht)
+    logger.info("Running check on VCF fields and info dict...")
+    if not check_vcf_field(ht, header_dict):
+        raise ValueError("Did not pass VCF field check")
+    output_path = (
+        f"{qc_temp_prefix(data_type=data_type)}gnomad.{data_type}.test{'.chr'+contig if contig else ''}.vcf.bgz"
+        if test
+        else release_vcf_path(contig=contig, data_type=data_type)
+    )
+    # Match header order to info field order
+    logger.info("Matching header order to info field order...")
+    ordered_vcf_info_dict = {
+        f: header_dict["info"][f] for f in list(ht.info) if f in header_dict["info"]
+    }
+    header_dict.update({"info": ordered_vcf_info_dict})
+
+    logger.info("Exporting VCF...")
+    export_reference = build_vcf_export_reference("gnomAD_GRCh38", keep_chrM=False)
+    hl.export_vcf(
+        rekey_new_reference(ht, export_reference),
+        output_path,
+        metadata=header_dict,
+        tabix=True,
+        overwrite=overwrite,
+    )
+
+
+def get_script_argument_parser() -> argparse.ArgumentParser:
+    """Get script argument parser."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--overwrite",
+        help="Overwrite existing files.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--test",
+        help="Export only the PCSK9 region for testing.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--contig",
+        help="Export only the specified contig.",
+        type=str,
+    )
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_script_argument_parser()
+    main(parser.parse_args())
