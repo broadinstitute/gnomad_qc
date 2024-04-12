5b2c95a3e2c0069ab15680dc4a299cc0b91403fc
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2024-04-12 11:19:47 -0600

    Format

--- gnomad_qc/v4/create_release/create_combined_faf_release_ht.py
+++ gnomad_qc/v4/create_release/create_combined_faf_release_ht.py
@@ -96,7 +96,7 @@ def extract_freq_info(
         - faf_meta: {prefix}_faf_meta
         - age_distribution: {prefix}_age_distribution
 
-    If `apply_release_filters` is True, a {prefix}_filters annotation is added to the 
+    If `apply_release_filters` is True, a {prefix}_filters annotation is added to the
     Table and the following variants are filtered:
         - chrM
         - AS_lowqual sites (these sites are dropped in the
