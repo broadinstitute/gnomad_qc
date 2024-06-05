a7b70644a4d8c5ff24eb0b4c98a8641a1eebe852
jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>
2024-06-05 14:39:40 -0600

    Update gnomad_qc/v4/assessment/calculate_per_sample_stats.py
    Co-authored-by: Qin He <44242118+KoalaQin@users.noreply.github.com>

--- gnomad_qc/v4/assessment/calculate_per_sample_stats.py
+++ gnomad_qc/v4/assessment/calculate_per_sample_stats.py
@@ -537,7 +537,7 @@ def main(args):
                 "Calculating per-sample variant statistics for %s...", data_type
             )
             filter_groups_ht = get_summary_stats_filtering_groups(
-                data_type, autosomes_only=autosomes_only
+                data_type, test=test, autosomes_only=autosomes_only
             ).ht()
             vds_load_func = (
                 get_gnomad_v4_vds
