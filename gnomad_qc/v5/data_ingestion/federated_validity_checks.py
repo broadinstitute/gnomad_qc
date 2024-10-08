7abcd7bd3c01126f0c8c5ebf251130ba5e47af33
Kristen Laricchia <kristen@broadinstitute.org>
2024-10-08 13:26:50 -0400

    PR suggestions

--- gnomad_qc/v5/data_ingestion/federated_validity_checks.py
+++ gnomad_qc/v5/data_ingestion/federated_validity_checks.py
@@ -7,6 +7,7 @@ import hail as hl
 
 from gnomad.assessment.validity_checks import summarize_variants
 from gnomad.resources.grch38.gnomad import public_release
+from gnomad_qc.v5.resources.basics import get_logging_path
 
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
@@ -21,29 +22,40 @@ def main(args):
         tmp_dir="gs://gnomad-tmp-4day",
     )
     hl.default_reference("GRCh38")
-    test = args.test
+    test_n_partitions = args.test_n_partitions
 
-    # TODO: Add resources to intake federated data once obtained.
-    if test:
+    try:
+        # TODO: Add resources to intake federated data once obtained.
         ht = public_release(data_type="exomes").ht()
-        ht = ht._filter_partitions(range(2))
 
-    expected_contigs = [
-        i
-        for i in hl.get_reference("GRCh38").contigs
-        if i in [f"chr{i}" for i in range(1, 23)] + ["chrX", "chrY"]
-    ]
-    logger.info("Summarizing variants and checking contigs.")
-    summarize_variants(ht, expected_contigs=expected_contigs)
+        if test_n_partitions:
+            ht = ht._filter_partitions(range(test_n_partitions))
+
+        expected_contigs = [
+            i
+            for i in hl.get_reference("GRCh38").contigs
+            if i in [f"chr{i}" for i in range(1, 23)] + ["chrX", "chrY"]
+        ]
+        logger.info("Summarizing variants and checking contigs.")
+        summarize_variants(ht, expected_contigs=expected_contigs)
+
+    finally:
+        logger.info("Copying hail log to logging bucket...")
+        hl.copy_log(get_logging_path("federated_validity_checks"))
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "--test",
-        help="Use the first two parititons of the gnomAD v4 exomes public release for testing.",
-        action="store_true",
+        "--test-n-partitions",
+        help=(
+            "Use only N partitions of the input for testing purposes. Defaults"
+            "to 2 if passed without a value."
+        ),
+        nargs="?",
+        const=2,
+        type=int,
     )
 
     args = parser.parse_args()
--- gnomad_qc/v5/resources/basics.py
+++ gnomad_qc/v5/resources/basics.py
@@ -6,3 +6,26 @@ from gnomad.resources.resource_utils import VariantDatasetResource
 dragen_tgp_vds = VariantDatasetResource(
     "gs://gnomad/v5.0/testing/genomes/dragen_tgp_v5.0_test.vds"
 )
+
+
+# TODO: Change default to CURRENT_VERSION.
+def qc_temp_prefix(version: str = "5.0") -> str:
+    """
+    Return path to temporary QC bucket.
+
+    :param version: Version of annotation path to return.
+    :return: Path to bucket with temporary QC data.
+    """
+    return f"gs://gnomad-tmp/gnomad.genomes.v{version}.qc_data/"
+
+
+# TODO: Change default to CURRENT_VERSION.
+def get_logging_path(name: str, version: str = "5.0") -> str:
+    """
+    Create a path for Hail log files.
+
+    :param name: Name of log file.
+    :param version: Version of annotation path to return.
+    :return: Output log path.
+    """
+    return f"{qc_temp_prefix(version)}{name}.log"
