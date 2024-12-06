5f9d73b53859c3b56b0ab99b613374f5d59ade77
Qin He <44242118+KoalaQin@users.noreply.github.com>
2024-12-06 12:48:11 -0500

    black formatting

--- gnomad_qc/v4/resources/basics.py
+++ gnomad_qc/v4/resources/basics.py
@@ -324,7 +324,10 @@ def get_gnomad_v4_vds(
                     filter_ht = filter_samples_ht.semi_join(filter_ht)
                 else:
                     filter_ht = filter_samples_ht.anti_join(filter_ht)
-                logger.info("Filtering VDS to %d samples in provided Table...", filter_ht.count()) 
+                logger.info(
+                    "Filtering VDS to %d samples in provided Table...",
+                    filter_ht.count(),
+                )
                 keep_samples = True
 
             filter_s = filter_ht.s.collect()
