b811c053ed650c75c5bf34d12a9eb080f8191ad9
Kristen Laricchia <kristen@broadinstitute.org>
2025-01-09 12:44:47 -0500

    small edit

--- gnomad_qc/v5/data_ingestion/federated_validity_checks.py
+++ gnomad_qc/v5/data_ingestion/federated_validity_checks.py
@@ -50,7 +50,7 @@ def check_missingness(
     :param ht: Input Table.
     :param missingness_threshold: Upper cutoff for allowed amount of missingness. Default is 0.50.
     :param struct_annotations: List of struct annotations to check for missingness. Default is ['grpmax', 'fafmax', 'histograms'].
-    :param indexed_array_annotations: Dictionary of indexed array struct annotations to check for missingness, where keys are the names of the annotation, and values are the names of the globals containing the mapping of group name to index for that key. Default is {'faf':'faf_index_dict', 'freq':'freq_index_dict'}.
+    :param indexed_array_annotations: Dictionary of indexed array struct annotations to check for missingness, where keys are the names of the annotations, and values are the names of the globals containing the mapping of group name to index for that key. Default is {'faf':'faf_index_dict', 'freq':'freq_index_dict'}.
     :return: None
     """
 
@@ -119,7 +119,7 @@ def validate_federated_data(
     :param ht: Input Table.
     :param missingness_threshold: Upper cutoff for allowed amount of missingness. Default is 0.50.
     :param struct_annotations_for_missingness: List of struct annotations to check for missingness. Default is ['grpmax', 'fafmax', 'histograms'].
-    :param indexed_array_annotations_for_missingness_dict: Dictionary of indexed array struct annotations to check for missingness, where keys are the names of the annotation, and values are the names of the globals containing the mapping of group name to index for that key. Default is {'faf':'faf_index_dict', 'freq':'freq_index_dict'}.
+    :param indexed_array_annotations_for_missingness_dict: Dictionary of indexed array struct annotations to check for missingness, where keys are the names of the annotations, and values are the names of the globals containing the mapping of group name to index for that key. Default is {'faf':'faf_index_dict', 'freq':'freq_index_dict'}.
     :return: None
     """
 
