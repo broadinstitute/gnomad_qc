0b94a0e56fb8976e76cbd346f5173a0d8c370f41
Kristen Laricchia <kristen@broadinstitute.org>
2025-01-31 10:14:04 -0500

    update config

--- gnomad_qc/v5/configs/validity_inputs_schema.py
+++ gnomad_qc/v5/configs/validity_inputs_schema.py
@@ -1,24 +1,20 @@
+"""Script defining schema for federated validty check input config file."""
+
 schema = {
     "type": "object",
     "properties": {
         "missingness_threshold": {"type": "number"},
         "indexed_array_annotations": {
             "type": "object",
-            "additionalProperties": {"type": "string"}  # Allow any string keys with string values
+            "additionalProperties": {"type": "string"},
         },
         "struct_annotations_for_missingness": {
             "type": "array",
-            "items": {"type": "string"}
+            "items": {"type": "string"},
         },
         "freq_meta_expr": {"type": "string"},
-        "freq_annotations_to_sum": {
-            "type": "array",
-            "items": {"type": "string"}
-        },
-        "freq_sort_order": {
-            "type": "array",
-            "items": {"type": "string"}
-        },
+        "freq_annotations_to_sum": {"type": "array", "items": {"type": "string"}},
+        "freq_sort_order": {"type": "array", "items": {"type": "string"}},
         "nhomalt_metric": {"type": "string"},
     },
     "required": [
@@ -28,7 +24,7 @@ schema = {
         "freq_meta_expr",
         "freq_annotations_to_sum",
         "freq_sort_order",
-        "nhomalt_metric"
+        "nhomalt_metric",
     ],
-    "additionalProperties": False
+    "additionalProperties": False,
 }
