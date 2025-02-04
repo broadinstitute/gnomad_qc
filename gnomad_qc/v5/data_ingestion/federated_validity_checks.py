87ae41ca8d8ad145b72ec676e57afab0e0697026
klaricch <kristen@broadinstitute.org>
2025-02-04 10:13:24 -0600

    Update gnomad_qc/v5/data_ingestion/federated_validity_checks.py
    Co-authored-by: Mike Wilson <mwilson@broadinstitute.org>

--- gnomad_qc/v5/data_ingestion/federated_validity_checks.py
+++ gnomad_qc/v5/data_ingestion/federated_validity_checks.py
@@ -101,7 +101,7 @@ def validate_federated_data(
     :param freq_meta_expr: Metadata expression that contains the values of the elements in
         `meta_indexed_expr`. The most often used expression is `freq_meta` to index into
         a 'freq' array (example: ht.freq_meta).
-    :param freq_annotations_to_sum: List of annotation fields withing `meta_expr` to sum. Default is ['AC', 'AN', 'homozygote_count']).
+    :param freq_annotations_to_sum: List of annotation fields within `meta_expr` to sum. Default is ['AC', 'AN', 'homozygote_count']).
     :param freq_sort_order: Order in which groupings are unfurled into flattened annotations. Default is ["gen_anc", "sex", "group"].
     :param nhomalt_metric: Name of metric denoting homozygous counts. Default is "nhomalt".
     :param verbose: If True, show top values of annotations being checked, including checks that pass; if False, show only top values of annotations that fail checks. Default is False.
