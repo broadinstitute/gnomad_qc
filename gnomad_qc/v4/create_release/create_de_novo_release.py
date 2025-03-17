7058d593939ec0fe87ef0b018d3f6e7c5699bafe
Qin He <44242118+KoalaQin@users.noreply.github.com>
2025-03-17 15:17:52 -0400

    Adjust docstring

--- gnomad_qc/v4/create_release/create_de_novo_release.py
+++ gnomad_qc/v4/create_release/create_de_novo_release.py
@@ -105,16 +105,9 @@ def aggregate_and_annotate_de_novos(ht: hl.Table) -> Tuple[hl.Table, hl.Table]:
 
     This step produces two sets of de novo calls:
 
-       - **Filtered de novos**: Variants that are not in low-confidence regions, do not have a `*` alt allele,
-       and are not excluded by variant QC. Includes all confidence levels.
+       - **Filtered de novos**: Variants that are not in low-confidence regions, do not have a `*` alt allele, and are not excluded by variant QC. Includes all confidence levels.
 
-       - **Filtered high-quality coding de novos**: A subset of filtered de novos, further restricted to:
-
-          - HIGH confidence, or MEDIUM confidence with a high P-value
-
-          - Coding consequence
-
-          - gnomAD v4.1 exomes allele frequency (AF) and callset allele count (AC) filters.
+       - **Filtered high-quality coding de novos**: A subset of filtered de novos, further restricted to HIGH confidence, or MEDIUM confidence with a high P-value, coding consequence, and gnomAD v4.1 exomes allele frequency (AF) and callset allele count (AC) filters.
 
     :param ht: De novo calls Table.
     :return: Aggregated and annotated Tables.
