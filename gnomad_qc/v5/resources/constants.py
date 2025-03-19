14dc20bb9511a3862ec737e78c403f40166d9298
Mike Wilson <mwilson@broadinstitute.org>
2025-03-19 16:28:52 -0400

    Drop gs:// prefix from workspace bucket in rwb

--- gnomad_qc/v5/resources/constants.py
+++ gnomad_qc/v5/resources/constants.py
@@ -1,6 +1,6 @@
 """Script containing version and release constants."""
 
 VERSIONS = ["5.0"]
-WORKSPACE_BUCKET = "gs://fc-secure-b25d1307-7763-48b8-8045-fcae9caadfa1"
+WORKSPACE_BUCKET = "fc-secure-b25d1307-7763-48b8-8045-fcae9caadfa1"
 CURRENT_VERSION = VERSIONS[-1]
 CURRENT_PROJECT_META_VERSION = "5.0"
