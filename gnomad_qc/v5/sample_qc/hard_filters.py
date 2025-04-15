7ee23fb1149b96570a708517e5bf7e80f984aec1
Qin He <44242118+KoalaQin@users.noreply.github.com>
2025-04-14 21:06:00 -0400

    Change logging path

--- gnomad_qc/v5/sample_qc/hard_filters.py
+++ gnomad_qc/v5/sample_qc/hard_filters.py
@@ -69,7 +69,7 @@ def main(args):
 
     finally:
         logger.info("Copying log to logging bucket...")
-        hl.copy_log(get_logging_path("hard_filters"))
+        hl.copy_log(get_logging_path("hard_filters", environment="rwb").path)
 
 
 def get_script_argument_parser() -> argparse.ArgumentParser:
