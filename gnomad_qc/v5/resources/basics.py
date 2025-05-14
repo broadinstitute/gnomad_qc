86846c601e9c4604626b41c12ea3c7f9743bba17
Qin He <44242118+KoalaQin@users.noreply.github.com>
2025-05-14 13:51:25 -0400

    Add Q30 base count in the filter

--- gnomad_qc/v5/resources/basics.py
+++ gnomad_qc/v5/resources/basics.py
@@ -275,6 +275,7 @@ def get_invalid_aou_samples() -> hl.Table:
         (ht.mean_coverage < 30)
         | (ht.genome_coverage < 0.9)
         | (ht.aou_hdr_coverage < 0.95)
+        | (ht.aligned_q30_bases < 0.8e11)
     ).select()
     logger.info("%s samples with low coverage...", low_cov_samples.count())
 
