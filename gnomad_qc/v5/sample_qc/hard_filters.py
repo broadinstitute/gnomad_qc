46d936eba65b6701c5ad12bcc0543cf0bdb2194f
Qin He <44242118+KoalaQin@users.noreply.github.com>
2025-05-15 22:24:23 -0400

    Improve logger info

--- gnomad_qc/v5/sample_qc/hard_filters.py
+++ gnomad_qc/v5/sample_qc/hard_filters.py
@@ -62,10 +62,16 @@ def compute_aou_sample_qc(
     vmt = vds.variant_data
 
     if test:
-        logger.info("Number of rows before filtering: %s", vmt.count_rows())
+        logger.info(
+            "Rows before filtering loci with >100 alt alleles: %s", vmt.count_rows()
+        )
+
     vmt = vmt.filter_rows(vmt.n_unsplit_alleles < 101)
+
     if test:
-        logger.info("Number of rows after filtering: %s", vmt.count_rows())
+        logger.info(
+            "Rows after filtering loci with >100 alt alleles: %s", vmt.count_rows()
+        )
 
     logger.info("Computing sample QC metrics...")
     sample_qc_ht = compute_stratified_sample_qc(
