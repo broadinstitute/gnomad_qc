e143183cc6bb9de8eea55732f22f58a19efb9fa0
Qin He <44242118+KoalaQin@users.noreply.github.com>
2025-05-16 20:42:36 -0400

    Correct tmp_dir

--- gnomad_qc/v5/sample_qc/hard_filters.py
+++ gnomad_qc/v5/sample_qc/hard_filters.py
@@ -88,7 +88,7 @@ def compute_aou_sample_qc(
 
 def main(args):
     """Determine samples that fail hard filtering thresholds."""
-    hl.init(tmp_dir=f"{WORKSPACE_BUCKET}/tmp/4_day")
+    hl.init(tmp_dir=f"gs://{WORKSPACE_BUCKET}/tmp/4_day")
     hl.default_reference("GRCh38")
 
     test = args.test
