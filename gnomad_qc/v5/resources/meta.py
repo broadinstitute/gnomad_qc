50df803ea1d126302397a5f2d3860d9e3d8d733e
ch-kr <kchao@broadinstitute.org>
2025-05-23 11:44:35 -0400

    add samples to exclude expressionresource to meta

--- gnomad_qc/v5/resources/meta.py
+++ gnomad_qc/v5/resources/meta.py
@@ -1,6 +1,10 @@
 """Script containing metadata related resources."""
 
-from gnomad.resources.resource_utils import TableResource, VersionedTableResource
+from gnomad.resources.resource_utils import (
+    ExpressionResource,
+    TableResource,
+    VersionedTableResource,
+)
 
 from gnomad_qc.v5.resources.constants import (
     CURRENT_PROJECT_META_VERSION,
@@ -20,3 +24,17 @@ project_meta = VersionedTableResource(
 sample_id_collisions = TableResource(
     path=f"gs://{WORKSPACE_BUCKET}/v5.0/metadata/gnomad.v5.0.sample_id_collisions.ht"
 )
+
+samples_to_exclude = ExpressionResource(
+    path=f"gs://{WORKSPACE_BUCKET}/v5.0/metadata/gnomad.v5.0.samples_to_exclude.he",
+)
+"""
+SetExpression containing IDs of samples to exclude from v5 analysis.
+
+Contains samples that should not have been included in the AoU v8 release
+(3 samples with unspecified quality issues and 4030 samples failing coverage hard filters)
+and 1490 samples with non-XX/XY sex ploidies.
+
+For more information about samples failing coverage hard filters, see
+docstring of `get_aou_failing_genomic_metrics_samples`.
+"""
