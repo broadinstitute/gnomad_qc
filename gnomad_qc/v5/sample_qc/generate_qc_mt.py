4fa5c4edbef5c80870ee91c0b2b530da06df35b4
Katherine Chao <kchao@broadinstitute.org>
2025-05-29 11:30:59 -0400

    Update gnomad_qc/v5/sample_qc/generate_qc_mt.py
    Co-authored-by: jkgoodrich <33063077+jkgoodrich@users.noreply.github.com>

--- gnomad_qc/v5/sample_qc/generate_qc_mt.py
+++ gnomad_qc/v5/sample_qc/generate_qc_mt.py
@@ -122,7 +122,7 @@ def generate_qc_mt(
 
 
 def main(args):
-    """Create a joint gnomAD + AoU dense MT of a diverse set of variants for relatedness/genetic ancestry PCA."""
+    """Create a joint gnomAD v4 (exomes + genomes) + AoU dense MT of a diverse set of variants for relatedness/genetic ancestry PCA."""
     hl.init(
         log="/home/jupyter/workspaces/gnomadproduction/generate_qc_mt.log",
         tmp_dir=f"gs://{WORKSPACE_BUCKET}/tmp/4_day",
