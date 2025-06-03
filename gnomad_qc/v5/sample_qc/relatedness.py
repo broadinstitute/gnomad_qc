80f6249515c36ab81ea7ab739e17745d3b159c01
ch-kr <kchao@broadinstitute.org>
2025-06-03 13:40:59 -0400

    add code to print cuKING command and convert QC MT to parquet

---
+++ gnomad_qc/v5/sample_qc/relatedness.py
@@ -0,0 +1,196 @@
+"""Script to compute relatedness estimates among pairs of samples in the joint gnomAD v4 (exomes + genomes) + AoU callset."""
+
+import argparse
+import logging
+import textwrap
+
+import hail as hl
+
+from gnomad_qc.resource_utils import check_resource_existence
+from gnomad_qc.v4.sample_qc.cuKING.mt_to_cuking_inputs import mt_to_cuking_inputs
+from gnomad_qc.v5.resources.basics import get_logging_path
+from gnomad_qc.v5.resources.constants import WORKSPACE_BUCKET
+from gnomad_qc.v5.resources.sample_qc import get_cuking_input_path
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger("relatedness")
+logger.setLevel(logging.INFO)
+
+
+def print_cuking_command(
+    cuking_input_path: str,
+    cuking_output_path: str,
+    min_emission_kinship: float = 0.5,
+    cuking_split_factor: int = 4,
+    location: str = "us-central1",
+    machine_type: str = "a2-highgpu-1g",
+    accelerator_count: int = 1,
+    accelerator_type: str = "nvidia-tesla-a100",
+    requester_pays_project: str = "terra-vpc-sc-93ccd8d2",
+) -> None:
+    """
+    Print the command to submit a dsub job for running cuKING.
+
+    :param cuking_input_path: Path to the cuKING input Parquet files.
+    :param cuking_output_path: Path to the cuKING output Parquet files.
+    :param min_emission_kinship: Minimum kinship threshold for emitting a pair of
+        samples in the relatedness output.
+    :param cuking_split_factor: Split factor to use for splitting the full relatedness
+        matrix table into equally sized submatrices that are computed independently to
+        parallelize the relatedness computation and decrease the memory requirements.
+        For example, to halve memory requirements, the full matrix can be split into
+        equally sized submatrices (i.e. a 'split factor' of 4). Only the
+        'upper triangular' submatrices need to be evaluated due to the symmetry of the
+        relatedness matrix, leading to 10 shards. Default is 4.
+    :param location: Location to run the dsub job. Default is "us-central1".
+    :param machine_type: Machine type to use for the dsub job. Default is "a2-highgpu-1g".
+    :param accelerator_count: Number of accelerators to use for the dsub job. Default is 1.
+    :param accelerator_type: Type of accelerator to use for the dsub job.
+        Default is "nvidia-tesla-a100".
+    :param requester_pays_project: Project ID to use for requester pays buckets.
+        Default is "terra-vpc-sc-93ccd8d2".
+    :return:
+    """
+    logger.info(
+        "Printing a command that can be used to submit a dsub job to run "
+        "cuKING on the files created by --prepare-cuking-inputs."
+    )
+    logger.warning(
+        "This printed command assumes that the cuKING directory is in the same "
+        "location where the command is being run and that $PROJECT_ID is set!"
+    )
+    SPLIT_FACTOR = cuking_split_factor
+    TOTAL_SHARDS = "$((SPLIT_FACTOR * (SPLIT_FACTOR + 1) / 2))"
+    print(
+        textwrap.dedent(
+            f"""\
+            for SHARD_INDEX in $(seq 0 $(({TOTAL_SHARDS} - 1))); do \\
+                cuKING_dsub \ \\
+                --location={location} \ \\
+                --machine-type={machine_type} \ \\
+                --accelerator-count={accelerator_count} \ \\
+                --accelerator-type={accelerator_type} \ \\
+                --command="cuking \ \\
+                --input_uri={cuking_input_path} \ \\
+                --output_uri={cuking_output_path} \ \\
+                --requester_pays_project={requester_pays_project} \ \\
+                --kin_threshold={min_emission_kinship} \ \\
+                --split-factor={SPLIT_FACTOR} \ \\
+                --shard-index=SHARD_INDEX" \\
+            done
+            """
+        )
+    )
+
+
+def main(args):
+    """Compute relatedness estimates among pairs of samples in the callset."""
+    test = args.test
+    overwrite = args.overwrite
+    min_emission_kinship = args.min_emission_kinship
+
+    if args.print_cuking_command:
+        print_cuking_command(
+            res.cuking_input_path,
+            res.cuking_output_path,
+            min_emission_kinship,
+            args.cuking_split_factor,
+        )
+        return
+
+    hl.init(
+        log="/home/jupyter/workspaces/gnomadproduction/relatedness.log",
+        tmp_dir=f"gs://{WORKSPACE_BUCKET}/tmp/4_day",
+    )
+    hl.default_reference("GRCh38")
+
+    joint_qc_mt = relatedness_resources.joint_qc_mt.mt()
+
+    try:
+        if args.prepare_cuking_inputs:
+            check_resource_existence(
+                output_step_resources={"cuking_input_parquet": get_cuking_input_path}
+            )
+            logger.info(
+                "Converting joint dense QC MatrixTable to a Parquet format suitable "
+                "for cuKING."
+            )
+            if test:
+                logger.info("Filtering MT to the first 2 partitions for testing...")
+                joint_qc_mt = joint_qc_mt._filter_partitions(range(2))
+
+            mt_to_cuking_inputs(
+                mt=joint_qc_mt,
+                parquet_uri=res.cuking_input_path,
+                overwrite=overwrite,
+            )
+
+    finally:
+        logger.info("Copying hail log to logging bucket...")
+        hl.copy_log(get_logging_path("relatedness"))
+
+
+def get_script_argument_parser() -> argparse.ArgumentParser:
+    """Get script argument parser."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "-o",
+        "--overwrite",
+        help="Overwrite output files.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--test",
+        help="Filter to the first 2 partitions for testing.",
+        action="store_true",
+    )
+
+    cuking_args = parser.add_argument_group(
+        "cuKING specific relatedness arguments",
+        "Arguments specific to computing relatedness estimates using cuKING.",
+    )
+    cuking_args.add_argument(
+        "--prepare-cuking-inputs",
+        help=(
+            "Converts the dense QC MatrixTable to a Parquet format suitable for cuKING."
+        ),
+        action="store_true",
+    )
+    print_cuking_cmd = cuking_args.add_argument_group(
+        "Print cuKING Cloud Batch job submission command",
+        "Arguments used to create the cuKING Cloud Batch job submission command "
+        "needed to run cuKING.",
+    )
+    print_cuking_cmd.add_argument(
+        "--print-cuking-command",
+        help="Print the command to submit a Cloud Batch job for running cuKING.",
+        action="store_true",
+    )
+    print_cuking_cmd.add_argument(
+        "--cuking-split-factor",
+        help=(
+            "Split factor to use for splitting the full relatedness matrix table "
+            "into equally sized submatrices that are computed independently to "
+            "parallelize the relatedness computation and decrease the memory "
+            "requirements. For example, to halve memory requirements, the full matrix "
+            "can be split into  equally sized submatrices (i.e. a 'split factor' of 4)."
+            " Only the 'upper triangular' submatrices need to be evaluated due to "
+            "the symmetry of the relatedness matrix, leading to 10 shards."
+        ),
+        default=4,
+        type=int,
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_script_argument_parser()
+    args = parser.parse_args()
+
+    if args.print_cuking_command and (args.prepare_cuking_inputs):
+        parser.error(
+            "--print-cuking-command can't be used simultaneously with other run modes."
+        )
+
+    main(args)
