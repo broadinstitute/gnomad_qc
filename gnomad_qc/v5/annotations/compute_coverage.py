9306bb6b3afa0228520bbe5ef99cdf158daea623
ch-kr <kchao@broadinstitute.org>
2025-07-16 11:55:26 -0400

    update coverage to do cov, an, qual hists together

--- gnomad_qc/v5/annotations/compute_coverage.py
+++ gnomad_qc/v5/annotations/compute_coverage.py
@@ -2,16 +2,16 @@
 
 import argparse
 import logging
-from typing import Optional
+from typing import List, Optional
 
 import hail as hl
 from gnomad.resources.grch38.reference_data import vep_context
 from gnomad.utils.annotations import (
     build_freq_stratification_list,
     generate_freq_group_membership_array,
+    qual_hist_expr,
 )
 from gnomad.utils.sparse_mt import (
-    compute_coverage_stats,
     compute_stats_per_ref_site,
     get_allele_number_agg_func,
     get_coverage_agg_func,
@@ -27,7 +27,6 @@ from gnomad_qc.v5.resources.constants import WORKSPACE_BUCKET
 
 # from gnomad_qc.resources.meta import meta
 from gnomad_qc.v5.resources.release import (
-    release_all_sites_an_tsv_path,
     release_coverage_path,
     release_coverage_tsv_path,
 )
@@ -80,6 +79,7 @@ def compute_all_release_stats_per_ref_site(
     Compute coverage, allele number, and quality histograms per reference site.
 
     .. note::
+
         Running this function prior to calculating frequencies removes the need for an additional
         densify for frequency calculations.
 
@@ -159,9 +159,9 @@ def compute_all_release_stats_per_ref_site(
 
     # Keep coverage stats from global adj grouping (index 0) only.
     ht = ht.annotate_globals(
-        coverage_stats_meta_sample_count=ht_globals.strata_sample_count[0],
+        coverage_stats_meta_sample_count=ht.strata_sample_count[0],
     )
-    cov_stats_expr = _cov_stats(ht.coverage_stats[0], ht_globals.sample_count)
+    cov_stats_expr = _cov_stats(ht.coverage_stats[0], ht.sample_count)
 
     ht = ht.transmute(**cov_stats_expr)
     return ht.annotate(qual_hists=ht.qual_hists[0])
@@ -186,7 +186,16 @@ def main(args):
     n_partitions = args.n_partitions
 
     try:
-        if args.compute_coverage_ht or args.compute_all_sites_an_and_qual_hist_ht:
+        # NOTE: v5 genomes coverage returns Table with coverage, all sites AN, and
+        # qual hists.
+        cov_and_an_ht_path = release_coverage_path(
+            public=False,
+            test=test,
+            include_meta=True,
+            coverage_type="coverage",
+        )
+
+        if args.compute_all_cov_release_stats_ht:
             # Read in context Table.
             ref_ht = vep_context.versions["105"].ht()
             if test_chr22_chrx_chry:
@@ -216,40 +225,11 @@ def main(args):
             #    chrom=["chr22", "chrX", "chrY"] if test_chr22_chrx_chry else None,
             # )
 
-        if args.compute_coverage_ht:
-            cov_ht_path = release_coverage_path(
-                public=False, test=test, include_meta=True, coverage_type="coverage"
-            )
-            check_resource_existence(output_step_resources={"coverage_ht": cov_ht_path})
-
-            logger.info("Running compute coverage...")
-            coverage_ht = compute_coverage_stats(
-                vds,
-                ref_ht,
-            )
-
-            # Checkpoint Table.
-            coverage_ht = coverage_ht.checkpoint(
-                hl.utils.new_temp_file("coverage", "ht")
-            )
-
-            # Naive coalesce and write out final Table.
-            coverage_ht = coverage_ht.naive_coalesce(n_partitions)
-            coverage_ht.write(cov_ht_path, overwrite=overwrite)
-
-        if args.compute_all_sites_an_and_qual_hist_ht:
-            an_ht_path = release_coverage_path(
-                public=False,
-                test=test,
-                include_meta=True,
-                coverage_type="allele_number",
+            logger.info(
+                "Running compute coverage, all sites allele number, and quality histograms HT..."
             )
             check_resource_existence(
-                output_step_resources={"all_sites_an_ht": an_ht_path}
-            )
-
-            logger.info(
-                "Running compute all sites allele number and quality histograms HT..."
+                output_step_resources={"coverage_and_an_ht": cov_and_an_ht_path}
             )
             group_membership_ht = get_genomes_group_membership_ht(
                 meta().ht(),
@@ -258,75 +238,48 @@ def main(args):
                 hl.utils.new_temp_file("group_membership", "ht")
             )
 
-            an_ht = compute_an_and_qual_hists_per_ref_site(
+            cov_and_an_ht = compute_all_release_stats_per_ref_site(
                 vds,
                 ref_ht,
                 group_membership_ht=group_membership_ht,
             )
-            an_ht = an_ht.checkpoint(hl.utils.new_temp_file("an", "ht"))
+            cov_and_an_ht = cov_and_an_ht.checkpoint(
+                hl.utils.new_temp_file("cov_and_an", "ht")
+            )
             # Naive coalesce and write out the intermediate HT.
-            an_ht = an_ht.naive_coalesce(n_partitions)
-            an_ht.write(an_ht_path.path, overwrite=overwrite)
+            cov_and_an_ht = cov_and_an_ht.naive_coalesce(n_partitions)
+            cov_and_an_ht.write(cov_and_an_ht_path, overwrite=overwrite)
 
         if args.export_coverage_release_files:
-            cov_ht_path = release_coverage_path(
-                public=False, test=test, include_meta=True, coverage_type="coverage"
-            )
             release_ht_path = release_coverage_path(
                 public=False, test=test, include_meta=False, coverage_type="coverage"
             )
-            cov_tsv_path = release_coverage_tsv_path(test=test)
+            release_tsv_path = release_coverage_tsv_path(test=test)
             check_resource_existence(
-                input_step_resources={"cov_ht": cov_ht_path},
+                input_step_resources={"cov_and_an_ht": cov_and_an_ht_path},
                 output_step_resources={
                     "cov_release_ht": release_ht_path,
-                    "cov_tsv": cov_tsv_path,
+                    "cov_tsv": release_tsv_path,
                 },
             )
 
-            logger.info("Exporting coverage tsv...")
-            ht = hl.read_table(cov_ht_path)
-            if "coverage_stats" in ht.row:
-                ht = ht.select(
-                    **{k: ht.coverage_stats[0][k] for k in ht.coverage_stats[0]}
-                )
-            ht = ht.drop("coverage_stats_meta", "coverage_stats_meta_sample_count")
-            ht = ht.checkpoint(release_ht_path, overwrite=overwrite)
-            ht.export(cov_tsv_path)
-
-        if args.export_all_sites_an_release_files:
-            an_ht_path = release_coverage_path(
-                public=False,
-                test=test,
-                include_meta=True,
-                coverage_type="allele_number",
+            logger.info("Exporting coverage and AN HT...")
+            # Select coverage and AN fields for release.
+            ht = hl.read_table(cov_and_an_ht_path)
+            ht = ht.select_globals(
+                an_strata_meta=ht.strata_meta,
+                an_strata_sample_count=ht.strata_sample_count,
             )
-            release_ht_path = release_coverage_path(
-                public=False,
-                test=test,
-                include_meta=False,
-                coverage_type="allele_number",
+            ht = ht.select(
+                "AN",
+                **{k: ht.coverage_stats[0][k] for k in ht.coverage_stats[0]},
             )
-            an_tsv_path = release_all_sites_an_tsv_path(test=test)
-            check_resource_existence(
-                input_step_resources={"an_ht": an_ht_path},
-                output_step_resources={
-                    "an_release_ht": release_ht_path,
-                    "an_tsv": an_tsv_path,
-                },
-            )
-
-            # Select only the AN and write out final Table.
-            logger.info("Exporting all sites AN HT...")
-            ht = hl.read_table(an_ht_path)
-            ht = ht.select("AN")
-
             ht = ht.checkpoint(release_ht_path, overwrite=overwrite)
 
-            logger.info("Exporting all sites AN tsv...")
+            logger.info("Exporting coverage and AN tsv...")
             # Only export the adj AN for all release samples.
-            ht = ht.annotate(AN=ht.AN[0])
-            ht.export(an_tsv_path)
+            ht = ht.transmute(AN=ht.AN[0])
+            ht.export(release_tsv_path)
 
     finally:
         logger.info("Copying hail log to logging bucket...")
@@ -365,18 +318,13 @@ def get_script_argument_parser() -> argparse.ArgumentParser:
         "--compute-coverage-ht", help="Compute the coverage HT.", action="store_true"
     )
     parser.add_argument(
-        "--compute-all-sites-an-and-qual-hist-ht",
-        help="Compute the all sites allele number and quality histogram HT.",
+        "--compute-all-cov-release-stats-ht",
+        help="Compute the all sites coverage, allele number, and quality histogram HT.",
         action="store_true",
     )
     parser.add_argument(
         "--export-coverage-release-files",
         help="Exports coverage release HT and TSV file.",
-        action="store_true",
-    )
-    parser.add_argument(
-        "--export-all-sites-an-release-files",
-        help="Export all sites AN release HT and TSV file.",
         action="store_true",
     )
 
