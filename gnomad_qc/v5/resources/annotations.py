fbc285cf7ba237c49c8a3e40f2c6447eb464dc56
ch-kr <kchao@broadinstitute.org>
2025-08-21 14:34:01 -0400

    add annotations resource file

---
+++ gnomad_qc/v5/resources/annotations.py
@@ -0,0 +1,36 @@
+"""Script containing annotation related resources."""
+
+from gnomad_qc.v5.resources.basics import qc_temp_prefix
+from gnomad_qc.v5.resources.constants import (
+    CURRENT_ANNOTATION_VERSION,
+    GNOMAD_BUCKET,
+    WORKSPACE_BUCKET,
+)
+
+
+def _annotations_root(
+    version: str = CURRENT_ANNOTATION_VERSION,
+    test: bool = False,
+    data_type: str = "genomes",
+    data_set: str = "rwb",
+) -> str:
+    """
+    Get root path to the variant annotation files.
+
+    :param version: Version of annotation path to return.
+    :param test: Whether to use a tmp path for analysis of the test VDS instead of the
+        full v4 VDS.
+    :param data_type: Data type of annotation resource. e.g. "exomes" or "genomes". Default is "genomes".
+    :param environment: Environment of annotation resource. Default is "rwb".
+    :return: Root path of the variant annotation files.
+    """
+    path_suffix = f"sample_qc/{data_type}/{data_set}"
+
+    if test:
+        environment = "rwb" if data_set == "aou" else "dataproc"
+        return (
+            f"{qc_temp_prefix(version=version, environment=environment)}{path_suffix}"
+        )
+
+    base_bucket = WORKSPACE_BUCKET if data_set == "aou" else GNOMAD_BUCKET
+    return f"gs://{base_bucket}/v{version}/{path_suffix}"
