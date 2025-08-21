f9adb5c5255a9448ac22383009f58975d5c1674c
ch-kr <kchao@broadinstitute.org>
2025-08-21 14:36:51 -0400

    add downsampling resource

--- gnomad_qc/v5/resources/annotations.py
+++ gnomad_qc/v5/resources/annotations.py
@@ -1,7 +1,10 @@
 """Script containing annotation related resources."""
 
+from gnomad.resources.resource_utils import TableResource, VersionedTableResource
+
 from gnomad_qc.v5.resources.basics import qc_temp_prefix
 from gnomad_qc.v5.resources.constants import (
+    ANNOTATION_VERSIONS,
     CURRENT_ANNOTATION_VERSION,
     GNOMAD_BUCKET,
     WORKSPACE_BUCKET,
@@ -34,3 +37,28 @@ def _annotations_root(
 
     base_bucket = WORKSPACE_BUCKET if data_set == "aou" else GNOMAD_BUCKET
     return f"gs://{base_bucket}/v{version}/{path_suffix}"
+
+
+def get_downsampling(test: bool = False, subset: str = "aou") -> VersionedTableResource:
+    """
+    Get the downsampling annotation table.
+
+    .. note::
+        v5 downsamplings only applies to the AoU dataset.
+
+    :param test: Whether to use a tmp path for tests. Default is False.
+    :param subset: Subset to return downsampling Table for. Default is "aou".
+    :return: Hail Table containing subset or overall dataset downsampling annotations.
+    """
+    if subset != "aou":
+        raise ValueError("v5 downsamplings only applies to the AoU dataset.")
+
+    return VersionedTableResource(
+        CURRENT_ANNOTATION_VERSION,
+        {
+            version: TableResource(
+                f"{_annotations_root(version, test=test)}/gnomad.genomes.v{version}.downsampling{f'.{subset}' if subset else ''}.ht"
+            )
+            for version in ANNOTATION_VERSIONS
+        },
+    )
