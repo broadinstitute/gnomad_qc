61844184e5910ee50c3b0592bfe4a13a56bca24d
ch-kr <kchao@broadinstitute.org>
2025-08-22 13:01:50 -0400

    remove release filter

--- gnomad_qc/v5/annotations/compute_coverage.py
+++ gnomad_qc/v5/annotations/compute_coverage.py
@@ -85,9 +85,6 @@ def get_genomes_group_membership_ht(meta_ht: hl.Table, ds_ht: hl.Table) -> hl.Ta
     :param ds_ht: Downsampling HT.
     :return: Group membership HT.
     """
-    # Filter to release samples.
-    meta_ht = meta_ht.filter(meta_ht.release)
-
     # TODO: remove this for v5.
     ds_ht = ds_ht.transmute(
         downsampling=ds_ht.downsampling.annotate(gen_idx=ds_ht.downsampling.pop_idx)
