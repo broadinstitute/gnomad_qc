2f2d4e00961e971f1a4f99ce076d0060dc562800
Mike Wilson <mwilson@broadinstitute.org>
2025-10-02 09:27:56 -0400

    reorder freq struct for join

--- gnomad_qc/v5/annotations/generate_frequency.py
+++ gnomad_qc/v5/annotations/generate_frequency.py
@@ -336,14 +336,14 @@ def _calculate_consent_frequencies(vmt: hl.MatrixTable, test: bool = False) -> h
         freq=hl.range(hl.len(consent_freq_ht.AC)).map(
             lambda i: hl.struct(
                 AC=hl.int32(consent_freq_ht.AC[i]),
-                homozygote_count=hl.int32(consent_freq_ht.homozygote_count[i]),
-                AN=hl.int(866 * 2),  # consent_ans_ht[consent_freq_ht.key].AN[i],
                 AF=hl.if_else(
                     consent_freq_ht.AC[i] > 0,
                     consent_freq_ht.AC[i]
                     / hl.float32(866 * 2),  # consent_ans_ht[consent_freq_ht.key].AN[i],
                     0.0,
                 ),
+                AN=hl.int(866 * 2),  # consent_ans_ht[consent_freq_ht.key].AN[i],
+                homozygote_count=hl.int32(consent_freq_ht.homozygote_count[i]),
             )
         )
     )
