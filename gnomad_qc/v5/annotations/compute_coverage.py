7584624e344302bc499f1a56419fc3046c27dca9
ch-kr <kchao@broadinstitute.org>
2025-10-22 13:28:03 -0400

    add missing release_only back

--- gnomad_qc/v5/annotations/compute_coverage.py
+++ gnomad_qc/v5/annotations/compute_coverage.py
@@ -702,6 +702,7 @@ def main(args):
             else:
                 sex_karyotype_field = "meta.sex_imputation.sex_karyotype"
                 vds = get_gnomad_v5_genomes_vds(
+                    release_only=True,
                     consent_drop_only=True,
                     test=test,
                     filter_partitions=range(2) if test_2_partitions else None,
