ab5151db9e5f8fbbacee5655223d1a7e20493a5e
ch-kr <kchao@broadinstitute.org>
2025-11-17 16:25:01 -0500

    add run_generate_trio_stats function

--- gnomad_qc/v5/annotations/generate_variant_qc_annotations.py
+++ gnomad_qc/v5/annotations/generate_variant_qc_annotations.py
@@ -4,13 +4,51 @@ import argparse
 import logging
 
 import hail as hl
+from gnomad.sample_qc.relatedness import filter_to_trios
+from gnomad.variant_qc.pipeline import generate_trio_stats
 
+from gnomad_qc.v5.annotations.annotation_utils import annotate_adj
 from gnomad_qc.v5.resources.basics import get_logging_path
 from gnomad_qc.v5.resources.constants import GNOMAD_TMP_BUCKET
 
 logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
 logger = logging.getLogger("generate_variant_qc_annotations")
 logger.setLevel(logging.INFO)
+
+
+def run_generate_trio_stats(
+    vds: hl.vds.VariantDataset,
+    fam_ped: hl.Pedigree,
+    fam_ht: hl.Table,
+) -> hl.Table:
+    """
+    Generate trio transmission stats from a VariantDataset and pedigree info.
+
+    :param vds: VariantDataset to generate trio stats from.
+    :param fam_ped: Pedigree containing trio info.
+    :param fam_ht: Table containing trio info.
+    :return: Table containing trio stats.
+    """
+    # Filter the VDS to autosomes and trios.
+    vds = hl.vds.filter_chromosomes(vds, keep_autosomes=True)
+    vds = filter_to_trios(vds, fam_ht)
+
+    vmt = vds.variant_data
+    rmt = vds.reference_data
+
+    # Filter the variant data to bi-allelic sites.
+    vmt = vmt.filter_rows(hl.len(vmt.alleles) == 2)
+
+    # Densify and annotate adj.
+    vds = hl.vds.VariantDataset(reference_data=rmt, variant_data=vmt)
+    mt = hl.vds.to_dense_mt(vds)
+    mt = mt.transmute_entries(GT=mt.LGT)
+    mt = annotate_adj(mt)
+
+    # Create trio matrix and generate trio stats.
+    mt = hl.trio_matrix(mt, pedigree=fam_ped, complete_trios=True)
+    # TODO: Check why this is False when we filter to bi-allelics above.
+    return generate_trio_stats(mt, bi_allelic_only=False)
 
 
 def main(args):
