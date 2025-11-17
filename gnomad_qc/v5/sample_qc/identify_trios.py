de4272902ee88d1f6e3616632d735dc5bf0e8782
ch-kr <kchao@broadinstitute.org>
2025-11-17 10:33:09 -0500

    initial commit of identify_trios script with args copied from v4

---
+++ gnomad_qc/v5/sample_qc/identify_trios.py
@@ -0,0 +1,172 @@
+"""Script to identify trios from relatedness data and filter based on Mendel errors and de novos."""
+
+import argparse
+import logging
+
+import hail as hl
+
+logging.basicConfig(format="%(levelname)s (%(name)s %(lineno)s): %(message)s")
+logger = logging.getLogger("identify_trios")
+logger.setLevel(logging.INFO)
+
+
+def main(args):
+    """Identify trios and filter based on Mendel errors and de novos."""
+    hl.init(
+        log="/identify_trios.log",
+        tmp_dir="gs://gnomad-tmp-4day",
+    )
+    hl.default_reference("GRCh38")
+
+    overwrite = args.overwrite
+    test = args.test
+
+    pass
+
+
+def get_script_argument_parser() -> argparse.ArgumentParser:
+    """Get script argument parser."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--overwrite",
+        help="Overwrite existing data.",
+        action="store_true",
+    )
+    parser.add_argument(
+        "--test",
+        help="Run mendel errors on only five partitions of the MT.",
+        action="store_true",
+    )
+
+    identify_dup_args = parser.add_argument_group("Duplicate identification")
+    identify_dup_args.add_argument(
+        "--identify-duplicates",
+        help=(
+            "Create a table with duplicate samples indicating which one is the best to"
+            " use based on the ranking of all samples after sample QC metric outlier "
+            "filtering (ranking used to determine related samples to drop for the "
+            "release)."
+        ),
+        action="store_true",
+    )
+
+    inter_fam_args = parser.add_argument_group("Pedigree inference")
+    inter_fam_args.add_argument(
+        "--infer-families",
+        help=(
+            "Infer families and trios using the relatedness Table and duplicate Table."
+        ),
+        action="store_true",
+    )
+
+    fake_ped_args = parser.add_argument_group("Fake Pedigree creation")
+    fake_ped_args.add_argument(
+        "--create-fake-pedigree",
+        help=(
+            "Create a fake Pedigree from unrelated samples in the data for comparison "
+            "to the inferred Pedigree."
+        ),
+        action="store_true",
+    )
+    fake_ped_args.add_argument(
+        "--fake-fam-prop",
+        help=(
+            "Number of fake trios to generate as a proportion of the total number of"
+            " trios found in the data. Default is 0.1."
+        ),
+        default=0.1,
+        type=float,
+    )
+
+    mendel_err_args = parser.add_argument_group("Mendel error calculation")
+    mendel_err_args.add_argument(
+        "--run-mendel-errors",
+        help="Calculate mendel errors for the inferred and fake Pedigrees on chr20.",
+        action="store_true",
+    )
+    finalize_ped_args = parser.add_argument_group(
+        "Pedigree filtering for final Pedigree generation"
+    )
+    finalize_ped_args.add_argument(
+        "--finalize-ped",
+        help=(
+            "Create final families/trios ped files by excluding trios where the number "
+            "of Mendel errors or de novos are outliers. Outliers can be defined as "
+            "trios that have Mendel errors or de novos higher than those specified in "
+            "--max-mendel and --max-de-novo respectively. They can also be defined as "
+            "trios that have Mendel errors or de novos higher than --max-mendel-z or "
+            "--max-de-novo-z standard deviations above the mean across inferred trios."
+        ),
+        action="store_true",
+    )
+    finalize_ped_args.add_argument(
+        "--max-mendel-z",
+        help=(
+            "Max number of standard deviations above the mean Mendel errors across "
+            "inferred trios to keep a trio. If flag is set, default is 3."
+        ),
+        nargs="?",
+        const=3,
+        type=int,
+    )
+    finalize_ped_args.add_argument(
+        "--max-de-novo-z",
+        help=(
+            "Max number of standard deviations above the mean de novos across inferred "
+            "trios to keep a trio. If flag is set, default is 3."
+        ),
+        nargs="?",
+        const=3,
+        type=int,
+    )
+    finalize_ped_args.add_argument(
+        "--max-mendel",
+        help=(
+            "Maximum number of raw Mendel errors for real trios. If specified and "
+            "--ukb-max-mendel is not, --max-mendel will be used for all samples. If "
+            "both --max-mendel and --ukb-max-mendel are specified, --max-mendel will "
+            "be used for non-UKB samples and --ukb-max-mendel will be used for UKB "
+            "samples."
+        ),
+        type=int,
+    )
+    finalize_ped_args.add_argument(
+        "--max-de-novo",
+        help=(
+            "Maximum number of raw de novo mutations for real trios. If specified and"
+            " --ukb-max-de-novo is not, --max-de-novo will be used for all samples. If"
+            " both --max-de-novo and --ukb-max-de-novo are specified, --max-de-novo"
+            " will be used for non-UKB samples and --ukb-max-de-novo will be used for"
+            " UKB samples."
+        ),
+        type=int,
+    )
+    finalize_ped_args.add_argument(
+        "--seed",
+        help=(
+            "Random seed for choosing one random trio per family to keep after "
+            "filtering."
+        ),
+        type=int,
+        default=24,
+    )
+
+    dense_trio_mt_args = parser.add_argument_group("Create dense trio MT.")
+    dense_trio_mt_args.add_argument(
+        "--create-dense-trio-mt",
+        help=("Create a dense MT for high quality trios."),
+        action="store_true",
+    )
+    dense_trio_mt_args.add_argument(
+        "--naive-coalesce-partitions",
+        help=("Number of partitions to coalesce the VDS to."),
+        type=int,
+        default=5000,
+    )
+
+    return parser
+
+
+if __name__ == "__main__":
+    parser = get_script_argument_parser()
+    main(parser.parse_args())
