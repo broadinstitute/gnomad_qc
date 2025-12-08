0c5079aba4e1bc12b31638432f7664cf89a39bf2
ch-kr <kchao@broadinstitute.org>
2025-12-08 12:22:39 -0500

    fix gnomad-tmp bucket

--- gnomad_qc/v5/annotations/generate_variant_qc_annotations.py
+++ gnomad_qc/v5/annotations/generate_variant_qc_annotations.py
@@ -49,7 +49,7 @@ def main(args):
     else:
         environment = "batch"
         hl.init(
-            tmp_dir=f"gs://{GNOMAD_TMP_BUCKET}/tmp/4_day",
+            tmp_dir=f"gs://{GNOMAD_TMP_BUCKET}-4_day",
             log="generate_variant_qc_annotations.log",
         )
     hl.default_reference("GRCh38")
