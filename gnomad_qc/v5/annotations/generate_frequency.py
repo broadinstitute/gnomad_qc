5d20925c11b748fac966647fb873d20fa8d2b908
Mike Wilson <mwilson@broadinstitute.org>
2026-01-09 12:50:14 -0500

    Drop literal

--- gnomad_qc/v5/annotations/generate_frequency.py
+++ gnomad_qc/v5/annotations/generate_frequency.py
@@ -852,12 +852,10 @@ def calculate_faf_and_grpmax_annotations(
 
     updated_freq_ht = updated_freq_ht.select_globals(
         freq_meta=updated_freq_ht.freq_meta,
-        freq_index_dict=make_freq_index_dict_from_meta(
-            hl.literal(updated_freq_ht.freq_meta)
-        ),
+        freq_index_dict=make_freq_index_dict_from_meta(updated_freq_ht.freq_meta),
         freq_meta_sample_count=updated_freq_ht.freq_meta_sample_count,
         faf_meta=faf_meta,
-        faf_index_dict=make_freq_index_dict_from_meta(hl.literal(faf_meta)),
+        faf_index_dict=make_freq_index_dict_from_meta(faf_meta),
         age_distribution=updated_freq_ht.age_distribution,
         aou_downsamplings=updated_freq_ht.aou_downsamplings,
     )
