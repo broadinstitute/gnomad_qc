f15b0a91ae40955057dd967a8021694c84c02892
Kristen Laricchia <kristen@broadinstitute.org>
2026-02-13 16:22:11 -0500

    reformat AS_SB_TABLE

--- gnomad_qc/v5/annotations/generate_variant_qc_annotations.py
+++ gnomad_qc/v5/annotations/generate_variant_qc_annotations.py
@@ -268,6 +268,12 @@ def main(args):
             )
             info_ht = hl.read_table(info_ht_path)
             # TODO: v4 split vcf does not have added pipe delimited annotations, but check if these are needed for VQSR and add pipe to AS_QUALapprox and AS_VarDP if needed.
+            # Reformat AS_SB_TABLE to be a nested array of arrays for proper use within the 'adjust_vcf_incompatible_types' function.
+            info_ht = info_ht.annotate(
+                AS_SB_TABLE=hl.array(
+                    [info_ht.info.AS_SB_TABLE[0:2], info_htht.info.AS_SB_TABLE[2:4]]
+                )
+            )
             info_ht = adjust_vcf_incompatible_types(
                 info_ht, pipe_delimited_annotations=[]
             )
