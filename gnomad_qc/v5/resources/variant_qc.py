8fbe24a6fc086eca29b46584b21fffbdfe16da81
ch-kr <kchao@broadinstitute.org>
2026-05-29 14:44:53 -0400

    add resources for control samples

--- gnomad_qc/v5/resources/variant_qc.py
+++ gnomad_qc/v5/resources/variant_qc.py
@@ -1,12 +1,14 @@
 """Script containing variant QC related resources for v5."""
 
+from gnomad.resources.resource_utils import VariantDatasetResource
+
 from gnomad_qc.v5.resources.basics import (
     _ALL_ENVIRONMENTS,
     _get_base_bucket,
     _validate_environment,
     qc_temp_prefix,
 )
-from gnomad_qc.v5.resources.constants import CURRENT_VARIANT_QC_VERSION
+from gnomad_qc.v5.resources.constants import AOU_BUCKET, CURRENT_VARIANT_QC_VERSION
 
 
 def _variant_qc_root(
@@ -34,3 +36,23 @@ def _variant_qc_root(
         )
 
     return f"gs://{_get_base_bucket(environment)}/v{version}/{path_suffix}"
+
+
+######################################################################
+# Control sample resources
+######################################################################
+
+
+control_samples_aou_gvcfs_bucket = (
+    f"gs://{AOU_BUCKET}/wgs/short_read/snpindel/aux/qc/control_samples/"
+)
+"""
+Bucket containing gVCFs from 8 Genomes-in-a-Bottle (GiaB) samples sequenced with the same protocol as the AoU v8 data.
+"""
+
+control_samples_vds = VariantDatasetResource(
+    f"{_variant_qc_root(test=False)}/control_samples.vds",
+)
+"""
+VDS containing 8 GiaB samples.
+"""
